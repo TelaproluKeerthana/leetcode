f38181442014cda21b5ba003a2e75ddb3b2de527
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2022-11-17 03:51:06 +0000

    Sync LeetCode submission - Rectangle Area (python3)

---
+++ my-folder/problems/rectangle_area/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
+        area1 = (ax2-ax1)*(ay2-ay1)
+        area2= (bx2-bx1)*(by2-by1)
+        intersection =0
+        if( bx1<=ax2 and  bx2>=ax1 and by1<=ay2  and by2>=ay1):
+            intersection = (min(ax2,bx2)-max(ax1,bx1))*(min(by2,ay2)-max(by1,ay1))
+        
+        return area1+area2-intersection
