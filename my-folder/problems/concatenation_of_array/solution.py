275a097c78df1587b183f961347a3dd426b9b855
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2024-07-02 01:49:56 +0000

    Sync LeetCode submission - Concatenation of Array (python3)

--- my-folder/problems/concatenation_of_array/solution.py
+++ my-folder/problems/concatenation_of_array/solution.py
@@ -1,17 +1,7 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans=[0] * (2 * len(nums))
-        # [ 0, 0, 0, 0, 0, 0]
-
-        for i in range(len(ans)):
-            # i -> 0 to 5
-            ans[i] = nums[i % len(nums)]
-            
+        return nums+nums
 
 
-           
-            
-        return ans
 
-            
         
