d49f04334f8578678d412e2ed75f882a6b90beac
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2024-07-02 03:13:23 +0000

    Sync LeetCode submission - Baseball Game (python3)

--- my-folder/problems/baseball_game/solution.py
+++ my-folder/problems/baseball_game/solution.py
@@ -1,17 +1,15 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
-        stack = []
+        stack=[]
         for i in range(len(operations)):
             if operations[i]=='C':
-                if len(operations)>0:
+                if  len(operations)>1:
                     stack.pop()
-
             elif operations[i]=='D':
                 if len(operations)>=1:
                     a=stack.pop()
                     stack.append(a)
-                    stack.append(2*a)
-
+                    stack.append(a*2)
             elif operations[i]=='+':
                 if len(operations)>2:
                     a=stack.pop()
@@ -21,13 +19,9 @@ class Solution:
                     stack.append(a+b)
             else:
                 stack.append(int(operations[i]))
-        sum=0
-        for i in range(len(stack)):
-            sum=sum+stack[i]
-        return sum 
+        
+        return sum(stack)
+        
+            
 
-               
-
-
-
-
+        
