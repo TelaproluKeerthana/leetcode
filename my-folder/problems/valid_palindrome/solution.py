6d0e39740ab372c04343811dc35f74d576bb6d66
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2024-09-03 01:58:01 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

--- my-folder/problems/valid_palindrome/solution.py
+++ my-folder/problems/valid_palindrome/solution.py
@@ -1,5 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s1 = (''.join(c for c in s if c.isalnum())).lower()
-        return s1==s1[::-1]
+        newstr=''
+        for i in s:
+            if i.isalnum():
+                newstr+=i
+
+        return (newstr.lower()==newstr[::-1].lower())
             
+
+
+        
