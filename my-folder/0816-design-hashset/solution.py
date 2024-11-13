23d1a01817008ded7b9c683cd2dfbc25f13ddf6d
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2024-11-13 19:05:03 +0000

    Sync LeetCode submission Runtime - 28 ms (76.11%), Memory - 21.5 MB (100.00%)

--- my-folder/0816-design-hashset/solution.py
+++ my-folder/0816-design-hashset/solution.py
@@ -1,43 +1,17 @@
-class Node:
-    def __init__(self,key):
-        self.key=key
-        self.next=None
-
-
 class MyHashSet:
 
     def __init__(self):
-        self.hm=[Node(0)] *10000
-    #creation of dummy node
+        self.n = 1000
+        self.arr =[[] for _ in range(self.n)] 
 
     def add(self, key: int) -> None:
-        curr=self.hm[key%len(self.hm)]
-        while(curr.next):
-            if curr.next.key==key:
-                return
-            curr=curr.next
-        curr.next=Node(key)
+        if not self.contains(key):
+            self.arr[key%self.n].append(key)
 
     def remove(self, key: int) -> None:
-            curr=self.hm[key%len(self.hm)]
-            while(curr.next):
-                if curr.next.key==key:
-                    curr.next=curr.next.next
-                    return
-                curr=curr.next
-    
-    def contains(self, key: int) -> bool:
-        curr=self.hm[key%len(self.hm)]
-        while(curr.next):
-            if curr.next.key==key:
-                return True
-            curr=curr.next
-        return False
+        if self.contains(key):
+            self.arr[key%self.n].remove(key)
         
 
-
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+    def contains(self, key: int) -> bool:
+        return key in self.arr[key%self.n]
