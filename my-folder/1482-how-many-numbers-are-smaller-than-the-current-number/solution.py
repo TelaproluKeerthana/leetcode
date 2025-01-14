839dc6107492ffc16ee9185ae662ce6ca4ada9ea
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-01-14 17:38:20 +0000

    Sync LeetCode submission Runtime - 1 ms (89.36%), Memory - 12.5 MB (67.74%)

--- my-folder/1482-how-many-numbers-are-smaller-than-the-current-number/solution.py
+++ my-folder/1482-how-many-numbers-are-smaller-than-the-current-number/solution.py
@@ -4,5 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        return [sorted(nums).index(i) for i in nums]
+        snums=sorted(nums)
+        length=len(nums)
+        l=0 
+        r=0
+        hm={}
+        while(r<length):
+            if(snums[r] not in hm):
+                hm[snums[r]]=l
+            r+=1
+            if (r<length and snums[r-1]!=snums[r]):
+                l=r
+
+        return [hm[i] for i in nums] 
+
+
         
