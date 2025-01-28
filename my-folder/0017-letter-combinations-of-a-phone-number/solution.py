46097b50f11489010f7d65e61aafa76cb645f580
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-01-28 23:14:15 +0000

    Sync LeetCode submission Runtime - 3 ms (5.54%), Memory - 17.8 MB (68.51%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.py
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.py
@@ -9,8 +9,6 @@ class Solution:
                     "8": "tuv",
                     "9": "wxyz"
                     }
-        if not digits:
-            return []
         # for i in range(0,len(digits),2):
         #     list1=keyboard[digits[i]]
         #     list2=keyboard[digits[i+1]] if i+1<len(digits) else ''
@@ -19,15 +17,21 @@ class Solution:
         #         for j in list2:
         #             combs.append(i+j)
         #     print(combs)
-        res=['']
-        for d in digits:
-            if d in keyboard:
-                new_res=[]
-                for comb in res:
-                    for letter in keyboard[d]:
-                        new_res.append(comb+letter)
-                res=new_res
+        
+        res=[]
+        def dfs(i,path):
+            if(len(digits)==len(path)):
+                res.append("".join(path))
+                return 
+            for digi in keyboard[digits[i]]:
+                dfs(i+1,path+digi)
+        if digits:
+            dfs(0,'')
         return res
+
+
+            
+
                 
 
            
