4b0a83cde76814b7b152e0358a93b3559cf10634
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-02 21:58:20 +0000

    Sync LeetCode submission Runtime - 55 ms (15.92%), Memory - 19.4 MB (38.24%)

--- my-folder/0417-pacific-atlantic-water-flow/solution.py
+++ my-folder/0417-pacific-atlantic-water-flow/solution.py
@@ -1,36 +1,28 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        pacific = atlantic = False
-
-        def dfs(r, c, prevVal):
-            nonlocal pacific, atlantic
-            if r < 0 or c < 0:
-                pacific = True
-                return
-            if r >= ROWS or c >= COLS:
-                atlantic = True
-                return
-            if heights[r][c] > prevVal:
-                return
-
-            tmp = heights[r][c]
-            heights[r][c] = float('inf')
-            for dx, dy in directions:
-                dfs(r + dx, c + dy, tmp)
-                if pacific and atlantic:
-                    break
-            heights[r][c] = tmp
-
-        res = []
-        for r in range(ROWS):
-            for c in range(COLS):
-                pacific = False
-                atlantic = False
-                dfs(r, c, float('inf'))
-                if pacific and atlantic:
-                    res.append([r, c])
-        return res
+        rows=len(heights)
+        cols=len(heights[0])
+        atl=set()
+        pac=set()
+        def dfs(r,c,visit,prevHeight):
+            if(min(r,c)<0 or r>=rows or c>=cols or (r,c) in visit or heights[r][c] < prevHeight):
+                return 
+            
+            visit.add((r,c))
+            dfs(r + 1, c, visit, heights[r][c])
+            dfs(r - 1, c, visit, heights[r][c])
+            dfs(r, c + 1, visit, heights[r][c])
+            dfs(r, c - 1, visit, heights[r][c])
         
+        for r in range(rows):
+            dfs(r,0,pac,heights[r][0])
+            dfs(r,cols-1,atl,heights[r][cols-1])
+        for c in range(cols):
+            dfs(rows-1,c,atl,heights[rows-1][c])
+            dfs(0,c,pac,heights[0][c])
+        res=[]
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) in pac and (r,c) in atl:
+                    res.append([r,c])
+        return res
