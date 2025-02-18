3eda3b56c786a91579f89d6a8175ba44bee2a3ed
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-18 23:06:33 +0000

    Sync LeetCode submission Runtime - 7 ms (97.86%), Memory - 18.4 MB (90.47%)

--- my-folder/0055-jump-game/solution.py
+++ my-folder/0055-jump-game/solution.py
@@ -3,6 +3,7 @@ class Solution:
         goal = len(nums) - 1
 
         for i in range(len(nums) - 2, -1, -1):
+            #we shift the goal post by 1 comparing if we can reach the goal starting at a particular index
             if i + nums[i] >= goal:
                 goal = i
         return goal == 0
