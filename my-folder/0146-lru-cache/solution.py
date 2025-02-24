bea81bb682aea058bb499dd6a61d2ba95988331f
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-24 00:29:34 +0000

    Sync LeetCode submission Runtime - 109 ms (85.46%), Memory - 79.4 MB (8.75%)

--- my-folder/0146-lru-cache/solution.py
+++ my-folder/0146-lru-cache/solution.py
@@ -1,26 +1,24 @@
+from collections import OrderedDict
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity=capacity
-        self.lru = {}
+        self.cap=capacity
+        self.cache=OrderedDict()
         
-
     def get(self, key: int) -> int:
-        if key not in self.lru:
+        if key not in self.cache:
             return -1
-        self.lru[key]=self.lru.pop(key)
-        return self.lru[key]
-        
 
+        self.cache.move_to_end(key)
+        return self.cache[key]
+        
     def put(self, key: int, value: int) -> None:
-        if key in self.lru:
-            self.lru.pop(key)
-        self.lru[key]=value
-
-        if len(self.lru)>self.capacity:
-            self.lru.pop(next(iter(self.lru)))
-
-        
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key]=value
+        if len(self.cache)>self.cap:
+            self.cache.popitem(last=False)
+            
         
 
 
