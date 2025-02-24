cd2b0c3334a2cb09ab9cbe04705975b8329cf65c
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-24 18:28:54 +0000

    Sync LeetCode submission Runtime - 5 ms (55.87%), Memory - 20 MB (28.42%)

---
+++ my-folder/0653-two-sum-iv---input-is-a-bst/solution.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        prev_visited=set()
+        def dfs(node):
+            if not node:
+                return False
+            y=k-node.val
+            if y in prev_visited:
+                return True
+            prev_visited.add(node.val)
+            return dfs(node.left) or dfs(node.right)
+        return dfs(root)
+            
+
+            
+
+        
