76f07a8ce3d1340bb3740036d0f71e42c1370004
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-24 17:16:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (20.81%)

--- my-folder/0496-next-greater-element-i/solution.py
+++ my-folder/0496-next-greater-element-i/solution.py
@@ -1,20 +1,11 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # def find_next_element(hm,val):
-        #     for key,value in hm.items():
-        #         if value==val:
-        #             return key
-        #     return 0 
         if len(nums2)==1:
             return [-1]
         res=[-1]*len(nums1)
         hm={val:i for i,val in enumerate(nums2)}
         for i,num in enumerate(nums1):
-            j=hm[num]+1
-            # if(need+1>=len(nums2)):
-            #     res[i]=-1
-            # nextelement = find_next_element(hm,need)
-            
+            j=hm[num]+1            
             while(j<len(nums2)):
                 if num<nums2[j]:
                     res[i]=nums2[j]
