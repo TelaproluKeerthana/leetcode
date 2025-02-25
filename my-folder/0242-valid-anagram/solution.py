1684e671bd2bee13a4642e7ba3d308ca6ef90986
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-25 15:15:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (24.86%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,19 +1,30 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_freqs={}
-        t_freqs={}
-        for i in s:
-            # if i in s_freqs:
-            s_freqs[i]=s_freqs.get(i,0)+1
-            #     s_freqs[i]+=1
-            # s_freqs[i]=1
+        # s_freqs={}
+        # t_freqs={}
+        # for i in s:
+        #     if i not in s_freqs:
+        #     # s_freqs[i]=s_freqs.get(i,0)+1
+        #         s_freqs[i]=1
+        #     s_freqs[i]+=1
 
-        for i in t:
-            # if i in t_freqs:
-            t_freqs[i]=t_freqs.get(i,0)+1
-            #     t_freqs[i]+=1
-            # t_freqs[i]=1
-        return s_freqs==t_freqs
+        # for i in t:
+        #     if i not in t_freqs:
+        #     # t_freqs[i]=t_freqs.get(i,0)+1
+        #         t_freqs[i]=1  
+        #     t_freqs[i]+=1
+
+        # return s_freqs==t_freqs
+        if len(s)!=len(t):
+            return False
+
+        chars=set(s)
+        for c in chars:
+            if(s.count(c)!=t.count(c)):
+                return False
+        return True
+            
+
 
 
         
