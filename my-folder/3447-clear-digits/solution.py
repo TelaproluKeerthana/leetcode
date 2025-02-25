e87583c29198629d51ad9cf8be7a8acbbb57c390
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-25 19:21:01 +0000

    Sync LeetCode submission Runtime - 3 ms (27.66%), Memory - 17.7 MB (77.71%)

--- my-folder/3447-clear-digits/solution.py
+++ my-folder/3447-clear-digits/solution.py
@@ -1,12 +1,11 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        i=0
-        while i<len(s):
-            if s[i] in '0123456789':
-                s=s[:i-1]+s[i+1:]
-                i=max(0,i-1) #ca34 ->where i in 2 position we encountered digit so we remove i and i-1 position left with c4 now the i should reset to max(0,i-1) so i=1 now when we encounter a digit again 
-            else:i+=1
-        return s
+        stack=[]
+        for i in s:
+            if i in '0123456789' and len(stack)!=0:
+                stack.pop()
+            else: stack.append(i)
+        return "".join(stack)
 
 
         
