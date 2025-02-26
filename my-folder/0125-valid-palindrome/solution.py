b2eee535c90b2a102f6ef4ca4676454fa4270a67
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-26 00:05:18 +0000

    Sync LeetCode submission Runtime - 3 ms (98.93%), Memory - 18 MB (82.86%)

--- my-folder/0125-valid-palindrome/solution.py
+++ my-folder/0125-valid-palindrome/solution.py
@@ -1,20 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        lower_string = s.lower()
-        L=0
-        R=len(s)-1
-        while(L<R):
-            if lower_string[L].isalnum() and lower_string[R].isalnum():
-                if lower_string[L]!=lower_string[R]:
-                    return False
-                L+=1
-                R-=1
-            if not lower_string[L].isalnum():
-                L+=1
-            if not lower_string[R].isalnum():
-                R-=1
-        return True
-
-
-
-        
+        for c in string.punctuation:
+            s=s.replace(c,"")
+        s=s.replace(" ","").lower()
+        return s==s[::-1]
