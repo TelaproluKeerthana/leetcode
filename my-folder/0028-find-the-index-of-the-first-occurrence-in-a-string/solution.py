270abd107d677cf34db94541893507f8cdd45a72
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-26 04:02:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (32.37%)

--- my-folder/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
+++ my-folder/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
@@ -1,6 +1,30 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle in haystack:
-            return haystack.index(needle)
+        for i in range(len(haystack)):
+            if haystack[i]==needle[0]:
+                if haystack[i:i+len(needle)]==needle:
+                    return i
         return -1
+        # if not needle:
+        #     return 0
+        # l=0
+        # needle_len=len(needle)
+        # for r in range(len(haystack)):
+        #     if haystack[r]==needle[l]:
+        #         l+=1
+        #         if(l==needle_len):
+        #             return (r-needle_len+1)
+        #     else:
+        #         if l>0:
+        #             r-=l
+        #             l=0
+        # return -1
+            
+# haystack = "sadbutsad"
+#              r
+#  needle = "sad"
+#             l
+
+
+
         
