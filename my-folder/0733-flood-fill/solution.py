bc60584f80ea3c2f8d1f85b4dc4c887155a67ef1
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-27 23:19:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.2 MB (27.20%)

---
+++ my-folder/0733-flood-fill/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        rows=len(image)
+        cols=len(image[0])
+        start_color=image[sr][sc]
+        if color==start_color:
+            return image
+        def dfs(r,c):
+            if(r>=rows or c>=cols or c<0 or r<0 or image[r][c]!=start_color):
+                return 
+            image[r][c]=color
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c-1)
+            dfs(r,c+1)
+        dfs(sr,sc)
+        return image
+
+        
