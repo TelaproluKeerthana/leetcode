bbb308a1410bb2ed381d9d217ea7f71166e7fc0c
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-28 22:27:36 +0000

    Sync LeetCode submission Runtime - 19 ms (54.11%), Memory - 18 MB (33.90%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,25 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s)==0:
-            return 0
-        if len(s)==1:
-            return 1
-        maxwindowlen=0
+        if len(s)<=1:
+            return len(s)
         window=set()
         L=0
+        max_len=0
         for R in range(len(s)):
             while s[R] in window:
                 window.remove(s[L])
                 L+=1
             window.add(s[R])
-            maxwindowlen=max(maxwindowlen,R-L+1)
-        return maxwindowlen
-        
-# window=set()
-# window=abc
-# s="abcabcbb"
-#      r 
-# wind=abc
+            max_len=max(max_len,R-L+1)
+        return max_len
 
-# maxwindowlength=3
+
         
