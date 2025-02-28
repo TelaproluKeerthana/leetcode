ae32bf9f85bd761cfdfc12aa47deb7db0346ca00
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-02-28 22:54:42 +0000

    Sync LeetCode submission Runtime - 268 ms (31.25%), Memory - 29.1 MB (5.74%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -1,23 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         total_islands=0
+        visit=set()
         rows=len(grid)
         cols=len(grid[0])
         def dfs(r,c):
-            if(min(r,c)<0 or r>=rows or c>=cols or grid[r][c]=="0"):
+            if(r<0 or c<0 or r>=rows or c>=cols or grid[r][c]=="0" or (r,c) in visit):
                 return 
-            #mark the visited node with 0 so you dont go there in next iteration
-            grid[r][c]="0"
+            visit.add((r,c))
             dfs(r+1,c)
             dfs(r-1,c)
             dfs(r,c+1)
             dfs(r,c-1)
+
         for r in range(rows):
             for c in range(cols):
-                if(grid[r][c]=="1"):
+                if grid[r][c]=="1" and (r,c) not in visit:
                     dfs(r,c)
-                    total_islands+=1
-
+                    total_islands += 1
         return total_islands
 
-        
+
+            
+            
