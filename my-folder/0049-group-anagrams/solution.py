6f02c25e6335e092825dcafb9cd23fa36eaf898b
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-01 20:02:21 +0000

    Sync LeetCode submission Runtime - 20 ms (25.60%), Memory - 22.9 MB (11.56%)

--- my-folder/0049-group-anagrams/solution.py
+++ my-folder/0049-group-anagrams/solution.py
@@ -1,17 +1,30 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hm={}
-        for i in strs:
-            arr=[0]*26
-            for j in i:
-                arr[ord(j)-ord('a')]+=1
-            key=tuple(arr)
-            if key not in hm:
-                hm[key]=[]
-            hm[key].append(i)
-        return list(hm.values())
+        hm=defaultdict(list)
 
-        
+        for s in strs:
+            key=[0]*26
+            for c in s:
+                key[ord(c)-ord('a')]+=1
+            immutable_key=tuple(key)
+            hm[immutable_key].append(s)
+        group=[]
+        for i in hm.values():
+            group.append(i)
+        return group
 
 
+            
+        print(hm)
+
+
+
+        #     keys=Counter(s)
+        #     key_pair=''
+        #     for key in keys:
+        #         key_pair+=key
+        #     key_pair=tuple(key_pair)
+        #     hm[key_pair]+=1 if key_pair in hm else 1
+        # print(hm)
+            
         
