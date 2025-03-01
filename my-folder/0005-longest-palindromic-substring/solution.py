6fc4da2c14b2ac47590af385f704bfdf1be53d24
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-01 23:19:28 +0000

    Sync LeetCode submission Runtime - 321 ms (47.29%), Memory - 18 MB (25.71%)

--- my-folder/0005-longest-palindromic-substring/solution.py
+++ my-folder/0005-longest-palindromic-substring/solution.py
@@ -1,30 +1,43 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        stringStart=0
-        reslen=0
-        
+        if(len(s)==1):
+            return s
+        l=0
+        longest=0
+        longest_ss=''
         for i in range(len(s)):
-            #for odd cases
-            l,r=i,i
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if (r-l+1>reslen):
-                    stringStart = l
-                    reslen = r-l+1
-                l-=1
-                r+=1
-            #for even cases
+            #for odd cases when stringlen is 
             l=i
-            r=i+1
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if (r-l+1>reslen):
-                    stringStart = l
-                    reslen = r-l+1
+            r=i
+            while(l>=0 and r<len(s) and s[l]==s[r]):
+                if( r-l+1 > longest):
+                    start_index=l
+                    #start_index keeps tracks of from which position we saw the longest substring
+                    longest=r-l+1
                 l-=1
                 r+=1
-        return s[stringStart:stringStart+reslen]
-
+                
             
 
-
-
+            # for even cases
+            l=i
+            r=i+1
+            while(l>=0 and r<len(s) and s[l]==s[r]):
+                if( r-l+1 > longest):
+                    start_index=l
+                    #start_index keeps tracks of from which position we saw the longest substring
+                    longest=r-l+1
+                l-=1
+                r+=1
+            
+        return s[start_index:start_index+longest]
         
+
+
+
+
+         
+        
+
+
+
