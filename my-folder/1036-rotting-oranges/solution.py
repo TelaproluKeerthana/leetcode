442490d832bf5f9eeb47d5350baa6bb4c4aa749b
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-02 03:35:38 +0000

    Sync LeetCode submission Runtime - 4 ms (46.92%), Memory - 17.8 MB (71.14%)

--- my-folder/1036-rotting-oranges/solution.py
+++ my-folder/1036-rotting-oranges/solution.py
@@ -1,44 +1,50 @@
-from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows=len(grid)
-        cols=len(grid[0])
-        fresh=0
-        time=0
-        queue=Deque()
-        directions=[[0,1],[1,0],[-1,0],[0,-1]]
+        que = deque()
+        fresh = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        total_time=0 
+
         for r in range(rows):
             for c in range(cols):
-                if(grid[r][c]==1):
-                    fresh+=1
-                if(grid[r][c]==2):
-                    queue.append((r,c))
-        while queue and fresh>0:
-            length=len(queue)
-            for i in range(length):
-                r,c=queue.popleft()
-                for rd,rc in directions:
-                    row=rd+r
-                    col=rc+c
-                    if (row in range(len(grid))
-                        and col in range(len(grid[0]))
-                        and grid[row][col] == 1
-                    ):
-                        grid[row][col] = 2
-                        queue.append((row, col))
-                        fresh -= 1
-            time += 1
-        return time if fresh == 0 else -1
+                if(grid[r][c] == 2):
+                    que.append((r,c))
+                elif(grid[r][c] == 1):
+                    fresh += 1
+
+        directions = [[1,0], [-1,0], [0,1], [0,-1]]
+
+        while fresh  > 0 and que :
+            
+            for _ in range(len(que)):
+                r,c = que.popleft()
+                for rd, cd in directions:
+                    r_idx = r + rd
+                    c_idx = c + cd
+                    if  r_idx < rows and 0 <= r_idx and  0 <= c_idx and c_idx < cols:
+                        if(grid[r_idx][c_idx] == 1):
+                            fresh -= 1
+                            grid[r_idx][c_idx] = 2
+                            que.append((r_idx, c_idx))
+            
+            total_time += 1
+        
+        if fresh != 0:
+            return -1
+        
+        return total_time
+
+# fresh=2-1-1
+# time=1+1+1+1
 
 
                 
 
+                    
 
-                 
-        return self.total_time
-        
 
-# grid = [2,1,1]
-#        [1,1,0]
-#        [0,1,1]
+
+
+
         
