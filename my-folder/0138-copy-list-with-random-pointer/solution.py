19658cf0913e3bc9eb5d2213466fecebb28d3bc0
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-02 23:07:07 +0000

    Sync LeetCode submission Runtime - 44 ms (38.96%), Memory - 18.7 MB (19.11%)

--- my-folder/0138-copy-list-with-random-pointer/solution.py
+++ my-folder/0138-copy-list-with-random-pointer/solution.py
@@ -10,20 +10,53 @@ class Node:
 class Solution:
     def __init__(self):
         self.visited = {}
+    
+    def get_copy_node(node):
+        if node:
+            if node in self.visited:
+                return self.visited[node]
+            else:
+                self.visited[node] = Node(node.val, None, None)
+                return self.visited[node]
+        return None
 
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
 
         if (head == None):
             return None
-        if head in self.visited:
-            return self.visited[head]
-        
-        node = Node(head.val , None, None)
+        def get_copy_node(node):
+            if node:
+                if node in self.visited:
+                    return self.visited[node]
+                else:
+                    self.visited[node] = Node(node.val, None, None)
+                    return self.visited[node]
+            return None
 
-        self.visited[head] = node
+        old_node = head
+        new_node = Node(old_node.val, None, None)
+        new_head = new_node
+        self.visited[old_node] = new_node
+        
+        while old_node != None:
+            if(old_node.random):
+                new_node.random = get_copy_node(old_node.random)
+
+     
+            if(old_node.next):
+                new_node.next = get_copy_node(old_node.next)
+                
+            
+            old_node = old_node.next
+            new_node = new_node.next
+        
+        return new_head
+        # node = Node(head.val , None, None)
+
+        # self.visited[head] = node
 
         
-        node.next = self.copyRandomList(head.next)
-        node.random = self.copyRandomList(head.random)
+        # node.next = self.copyRandomList(head.next)
+        # node.random = self.copyRandomList(head.random)
         
-        return node        
+        # return node        
