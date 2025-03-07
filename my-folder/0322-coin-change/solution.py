7942d590396136fa54d4c594add2869252c35506
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-07 23:34:34 +0000

    Sync LeetCode submission Runtime - 1173 ms (13.89%), Memory - 46 MB (6.22%)

--- my-folder/0322-coin-change/solution.py
+++ my-folder/0322-coin-change/solution.py
@@ -1,10 +1,22 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        #setting a max value for each amount value in cache
-        dp= [amount+1] * (amount+1)
-        dp[0]=0
-        for a in range(1,amount+1):
-            for c in coins:
-                if a-c >=0:
-                    dp[a]=min(dp[a],1+dp[a-c])
-        return dp[amount] if dp[amount]!=amount+1 else -1
+        memo = {}
+        def dfs(amount):
+            if(amount == 0):
+                return 0
+            if amount in memo:
+                return memo[amount]
+            res = float("inf")
+            for coin in coins:
+                if amount - coin >= 0:
+                    res = min( 1 + dfs(amount - coin), res)
+            memo[amount]= res
+            return res
+        minCoins = dfs(amount)
+        return -1 if minCoins == float("inf") else minCoins
+
+
+            
+            
+
+        
