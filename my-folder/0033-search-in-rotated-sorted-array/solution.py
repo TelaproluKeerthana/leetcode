14a3122d80ea369beda28e8769d5e2987a1c58d3
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-07 03:46:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.2 MB (41.10%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.py
+++ my-folder/0033-search-in-rotated-sorted-array/solution.py
@@ -1,23 +1,22 @@
-class Solution(object):
-    def search(self, nums, target):
-        l=0
-        r=len(nums)-1
-        while l<=r :
-            mid=(l+r)//2
-            if target==nums[mid]:
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        #binary search
+        l = 0 
+        r = len(nums) - 1
+        while(l <= r):
+            mid = (l + r) // 2
+            if nums[mid] == target:
                 return mid
-            if nums[l]<=nums[mid] :
-                if nums[mid]<target or nums[l]>target:
-                    l=mid+1
+            if nums[l] <= nums[mid]:
+                if nums[mid] < target or nums[l] > target:
+                    l = mid + 1
                 else:
-                    r=mid-1
+                    r= mid - 1
             else:
-                if nums[mid]>target or nums[r]<target:
-                    r=mid-1
+                if nums[mid] > target or nums[r] < target:
+                    r = mid - 1
                 else:
-                    l=mid+1
+                    l = mid + 1 
+
         return -1
-
-
-
         
