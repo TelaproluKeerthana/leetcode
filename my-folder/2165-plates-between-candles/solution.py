baa75021e122c3a5b663be0a1e703ce03d419f07
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-09 21:51:00 +0000

    Sync LeetCode submission Runtime - 93 ms (82.75%), Memory - 49.3 MB (25.33%)

--- my-folder/2165-plates-between-candles/solution.py
+++ my-folder/2165-plates-between-candles/solution.py
@@ -4,8 +4,8 @@ class Solution:
         res = []
         
         total_stars = [0] * len(s)
-        left_bar = [-1] * len(s)
-        right_bar = [-1] * len(s)
+        closest_right_bar = [-1] * len(s)
+        closest_left_bar = [-1] * len(s)
         
         stars = 0
         for i in range(s_length):
@@ -17,21 +17,20 @@ class Solution:
         for i in range(s_length):
             if s[i] == "|":
                 first_bar = i
-            left_bar[i] = first_bar
+            closest_right_bar[i] = first_bar
         
         last_bar = -1
         for i in range(s_length - 1, -1, -1):
             if s[i] == "|":
                 last_bar = i
-            right_bar[i] = last_bar 
+            closest_left_bar[i] = last_bar 
         
         for l,r in queries:
-            left =  right_bar[l]
-            right = left_bar[r]
+            left =  closest_left_bar[l]
+            right = closest_right_bar[r]
             if (left != -1 and right != -1 and left < right):
-                stars_count = total_stars[right - 1] - (total_stars[left] if left > 0 else 0)
-                res.append(stars_count)
-                
+                stars_count = total_stars[right] - total_stars[left]
+                res.append(stars_count)  
             else:
                 res.append(0)
         return res
@@ -47,12 +46,6 @@ class Solution:
 # queries = [[2,5],[5,9]]
 # prefix = [1 2 2 3 4 4 5 6 7 7]
 #               l     5
-
-
-
-
-
-
 
 # s = * * | * * | * * * |
 #         l     7
