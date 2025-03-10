bb84638a831358746f972a35364ff8a57543ffd0
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-10 01:46:13 +0000

    Sync LeetCode submission Runtime - 10 ms (47.85%), Memory - 18.5 MB (92.13%)

--- my-folder/0045-jump-game-ii/solution.py
+++ my-folder/0045-jump-game-ii/solution.py
@@ -1,15 +1,18 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        l,r=0,0
-        res=0 #to track the total number of steps we make
-        while(r<len(nums)-1):
-            #keep track of the max element in that level
-            maxtravel=0
-            for i in range(l,r+1):
-                maxtravel=max(maxtravel,i+nums[i])
-            l=r+1
-            r=maxtravel
-            res+=1
+        l = 0
+        r = 0
+        total_steps = 0
+        while(r < len(nums) - 1):
+            maxtravel = 0
+            for level in range(l, r + 1):
+                maxtravel = max( level + nums[level], maxtravel )
 
-        return res
+            l = r + 1
+            r = maxtravel
+            total_steps += 1
+
+        return total_steps
+    
+
         
