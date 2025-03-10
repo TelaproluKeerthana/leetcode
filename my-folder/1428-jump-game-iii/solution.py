2053fe24ff2a366920a2f9ff0a028ce88f74d5a9
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-10 02:31:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22.9 MB (68.07%)

--- my-folder/1428-jump-game-iii/solution.py
+++ my-folder/1428-jump-game-iii/solution.py
@@ -2,22 +2,24 @@ class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         if arr[start] == 0:
             return True
+        queue = [start]
+        visited = set()
+        while queue:
+            node = queue.pop(0)
+            if node in visited:
+                continue
+            visited.add(node)
+            if (arr[node] + node < len(arr)):
+                if (arr[arr[node] + node] == 0):
+                    return True
+                queue.append(arr[node] + node)
+            if (node - arr[node] > -1):
+                if (arr[arr[node] - node] == 0):
+                    return True
+                queue.append(node - arr[node])
         
-        visited = [False] * len(arr)
-        
-        def dfs(idx):
-            
-            if (idx < 0 or idx >= len(arr) or visited[idx]):
-                return False
-            
-            if (arr[idx] == 0):
-                return True
-            visited[idx] = True
+        return False
 
-            right = dfs(idx + arr[idx]) 
-            left = dfs(idx - arr[idx])
-                
-            return left or right
 
-        return dfs(start)
+
         
