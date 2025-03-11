716dea687d8c3462bb228d07c1abb380bce6d922
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-11 17:41:56 +0000

    Sync LeetCode submission Runtime - 121 ms (13.53%), Memory - 18.7 MB (98.57%)

--- my-folder/0227-basic-calculator-ii/solution.py
+++ my-folder/0227-basic-calculator-ii/solution.py
@@ -1,43 +1,48 @@
 class Solution:
-    def calculate(self, s: str) -> int:
-        i = 0
-        # to keep track of the curr position
-        curr = prev = res = 0
-        curr_operation = '+'
-        while(i < len(s)):
-            curr_char = s[i]
+        def calculate(self, s: str) -> int:
 
-            if(curr_char.isdigit()):
-                while i < len(s) and s[i].isdigit():
-                    curr = curr * 10 + int(s[i])
-                    i += 1
-
-                i -= 1
-
-                if curr_operation == '+':
-                    res += curr
-                    prev = curr
-         #storing prev so if we encounter a * or / then you use prev and start computing it again
-                elif curr_operation == '-':
-                    res -= curr
-                    prev = -curr
-                elif curr_operation == '*':
-                    res -= prev
-                    res += curr * prev
-
-                    prev = curr * prev
-                else:
-                    res -= prev
-                    res += int(prev / curr)
-                    prev = int(prev / curr)
-                
-                curr = 0
-                #must reset to 0 after performing the operation
-            elif curr_char != " ":
-                curr_operation = curr_char
+            i = 0
+            curr = prev = res = 0  # Initialize result, current number, and previous number
+            curr_operation = '+'  # Start with addition as the default operation
             
-            i += 1
-        return res
+            while i < len(s):
+                curr_char = s[i]
+
+                # If the character is a digit, build the full number (handles multi-digit numbers)
+                if curr_char.isdigit():
+                    while i < len(s) and s[i].isdigit():
+                        curr = curr * 10 + int(s[i])  # Construct the number
+                        i += 1
+
+                    i -= 1  # Adjust index back since loop moves one extra step
+                    
+                    # Perform the previous stored operation
+                    if curr_operation == '+':
+                        res += curr
+                        prev = curr  # Store the last added value
+                    elif curr_operation == '-':
+                        res -= curr
+                        prev = -curr  # Store as negative for later corrections
+                    elif curr_operation == '*':
+                        # Undo previous operation, then apply multiplication
+                        res -= prev
+                        res += prev * curr
+                        prev = prev * curr  # Update prev to reflect new multiplication
+                    elif curr_operation == '/':
+                        # Undo previous operation, then apply integer division
+                        res -= prev
+                        res += int(prev / curr)
+                        prev = int(prev / curr)  # Update prev with new division result
+
+                    curr = 0  # Reset current number for the next operation
+                
+                # If the character is an operator (excluding spaces), update the operation to perform next
+                elif curr_char != " ":
+                    curr_operation = curr_char
+                
+                i += 1  # Move to the next character
+
+            return res
 
                 
 
