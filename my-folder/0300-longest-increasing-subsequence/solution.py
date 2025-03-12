740f8af81ebf9a7511b5406a961afdcc182a8259
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-12 01:44:01 +0000

    Sync LeetCode submission Runtime - 1820 ms (21.94%), Memory - 18.2 MB (18.93%)

--- my-folder/0300-longest-increasing-subsequence/solution.py
+++ my-folder/0300-longest-increasing-subsequence/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         freqCounter=[1]*len(nums)
-        for i in range(len(nums)-1,-1,-1):
-            for j in range(i+1, len(nums)): 
-                if nums[i] < nums[j]:
+        for i in range(1,len(nums)):
+            for j in range(i): 
+                if nums[j] < nums[i]:
                     freqCounter[i] = max(1+freqCounter[j], freqCounter[i]) 
+        print(freqCounter)
         return max(freqCounter)
                 
 
