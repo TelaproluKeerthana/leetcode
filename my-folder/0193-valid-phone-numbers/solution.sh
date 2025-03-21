f995e5020024a0fa30743b003fd262fa5999054d
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-21 14:10:57 +0000

    Sync LeetCode submission Runtime - 60 ms (78.66%), Memory - 3.5 MB (43.32%)

--- my-folder/0193-valid-phone-numbers/solution.sh
+++ my-folder/0193-valid-phone-numbers/solution.sh
@@ -1,3 +1,8 @@
 # Read from the file file.txt and output all valid phone numbers to stdout.
 grep -E '^(\([0-9]{3}\) [0-9]{3}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4})$' file.txt
-
+# grep to search pattern
+# E for evaluate regex expression
+# ^ for start of the string
+# $ end of the string
+# \( to include ( as its also an expression
+# [0 - 9]{3} 0-9 * 3 times 
