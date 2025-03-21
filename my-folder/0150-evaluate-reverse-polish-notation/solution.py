02def7d721d852cbbf98250156aa5e80f541fca5
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-21 02:02:07 +0000

    Sync LeetCode submission Runtime - 7 ms (12.63%), Memory - 19.3 MB (16.69%)

--- my-folder/0150-evaluate-reverse-polish-notation/solution.py
+++ my-folder/0150-evaluate-reverse-polish-notation/solution.py
@@ -1,24 +1,23 @@
 class Solution:
-    def evalRPN(self, tokens):
-        stack=[]
-        for i in tokens:
-            if i=="+":
-                stack.append(stack.pop()+stack.pop())
-            elif i=="-":
-                a,b=stack.pop(),stack.pop()
-                stack.append(b-a)
-            elif i=="*":
-                stack.append(stack.pop()*stack.pop())
-            elif i=="/":
-                a,b=stack.pop(),stack.pop()
-                stack.append(int(float (b)/a))
+    def evalRPN(self, tokens: List[str]) -> int:
+        # use a stack to store the digits when a operator comes do the operation and push result into the stack
+        stack = []
+        operators = set(["+", "-", "*", "/"])
+        for token in tokens:
+            if token not in operators:
+                stack.append(int(token))
             else:
-                stack.append(int(i))
+                if len(stack) >= 2:
+                    digit2 = stack.pop()
+                    digit1 = stack.pop()
+                    print(digit1, digit2)
+                    if token == "+":
+                        stack.append(digit1 + digit2)
+                    elif token == "-":
+                        stack.append(digit1 - digit2)
+                    elif token == "*":
+                        stack.append(digit1 * digit2)
+                    elif token == "/":
+                        stack.append(int(float(digit1) / digit2))
+        return stack[-1]
 
-        return stack[0]
-                 
-
-
-
-        
-        
