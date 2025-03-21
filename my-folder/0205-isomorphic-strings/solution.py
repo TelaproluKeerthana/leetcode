d7485ba179a0080a6a7a68a6dfc2eb0656955c51
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-21 16:42:34 +0000

    Sync LeetCode submission Runtime - 7 ms (57.72%), Memory - 17.8 MB (95.52%)

--- my-folder/0205-isomorphic-strings/solution.py
+++ my-folder/0205-isomorphic-strings/solution.py
@@ -2,12 +2,11 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         map_s_t = {}
         map_t_s = {}
-        for c1, c2 in zip(s, t):
-            if c1 not in map_s_t and c2 not in map_t_s:
-                map_s_t[c1] = c2
-                map_t_s[c2] = c1
-            elif map_s_t.get(c1) != c2 or map_t_s.get(c2) != c1:
+        for char_s, char_t in zip(s,t):
+            if (char_s in map_s_t and map_s_t[char_s] != char_t) or (char_t in map_t_s and map_t_s[char_t] != char_s):
                 return False
-        return True
 
+            map_s_t[char_s] = char_t
+            map_t_s[char_t] = char_s
+        return True
         
