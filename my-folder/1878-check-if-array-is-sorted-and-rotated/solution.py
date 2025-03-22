3679f4ceec510b649efcb308340b3d94e0491d06
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-22 22:29:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (17.24%)

--- my-folder/1878-check-if-array-is-sorted-and-rotated/solution.py
+++ my-folder/1878-check-if-array-is-sorted-and-rotated/solution.py
@@ -7,16 +7,23 @@ class Solution:
         for i, curr in enumerate(nums):
             if(prev > curr):
                 min_idx = i
+                break
             prev = curr
+        # nums[min_idx: ] + nums[0 : min_idx]
+        initial_pos = min_idx
+        while ((min_idx + 1) % len(nums) != initial_pos):
+            curr = nums[min_idx]
+            nxt = nums[(min_idx + 1) % len(nums)]
+            if curr > nxt:
+                return False
+            min_idx = (min_idx + 1) % len(nums)
         
-        if min_idx > 0:
-            return nums[min_idx: ] + nums[0 : min_idx] == sorted(nums)
-        
-        return nums == sorted(nums)
+        return True
 
-# 3,4,5,1,2
-# min = 1
-# index(1) = 3
-# return [3:] + [0:2] == sorted(nums)
-
+# min_idx = 1
+# 2 != 1
+# 0 + 1 != 1
+# [2, 1, 3, 4]
+#        
+   
         
