6e116d38984985fd526afa538df84fb2b105592e
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-24 14:15:59 +0000

    Sync LeetCode submission Runtime - 45 ms (42.31%), Memory - 18.2 MB (39.66%)

--- my-folder/0572-subtree-of-another-tree/solution.py
+++ my-folder/0572-subtree-of-another-tree/solution.py
@@ -1,38 +1,37 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isSubtree(self, root, subRoot):
-        """
-        :type root: Optional[TreeNode]
-        :type subRoot: Optional[TreeNode]
-        :rtype: bool
-        """
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if not subRoot:
             return True
         if not root:
-            return  False
+            return False
+        
         if self.isSameTree(root,subRoot):
             return True
-        return (self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot))
         
-    # def sameTree(self, newroot,subroot):
-    #     if not newroot and not subroot:
-    #         return  True
-    #     if newroot and subroot and newroot.val==subroot.val:
-    #         return (self.sameTree(newroot.left,subroot.left) and 
-    #         (self.sameTree(newroot.right,subroot.right))
-    #     return False
-    def isSameTree(self,left,right):
-        if not left and not right:
-            return True
-        if left and right and left.val==right.val:
-            return (self.isSameTree(left.left,right.left) 
-            and self.isSameTree(left.    right,right.right))
-        return False
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
+    def isSameTree(self, node, subnode):
+        if not subnode and not node:
+            return True
+    
+        if node and subnode and node.val == subnode.val:
+            return (self.isSameTree(node.left, subnode.left) and
+             self.isSameTree(node.right, subnode.right))    
+        return False
+    
         
+# dfs(3,4) -> true
+# dfs(4,4) or dfs(5,4) -> true or 
+# dfs(4,4) -> dfs(1,1) and dfs(2,2) -> true
+#           -> dfs(1,1) -> dfs(null,null) and dfs(null,null) -> true and true
+#           -> dfs(2,2) ->  true
+
+            
+
         
