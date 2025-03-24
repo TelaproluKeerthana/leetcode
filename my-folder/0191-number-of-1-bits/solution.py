86e29942a10d4261c3e16d36479c8531e4232828
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-24 18:18:14 +0000

    Sync LeetCode submission Runtime - 2 ms (18.76%), Memory - 17.8 MB (31.31%)

--- my-folder/0191-number-of-1-bits/solution.py
+++ my-folder/0191-number-of-1-bits/solution.py
@@ -1,9 +1,12 @@
 class Solution:
+    @lru_cache(maxsize = None)
     def hammingWeight(self, n: int) -> int:
         count = 0
-        for i in range(32):
-            count += n >> i & 1
+        while n:
+            n = n & (n - 1)
+            count += 1
         
         return count
+
 
         
