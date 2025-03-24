fc7bda7346dfd9dcd2f8d053f0c6aaa5aa503bd2
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-24 02:49:17 +0000

    Sync LeetCode submission Runtime - 3 ms (49.78%), Memory - 18.9 MB (33.81%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,29 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hm={val:i for i,val in enumerate(nums)}
-        for i in range(len(nums)):
-            req_val=target-nums[i]
-            if req_val in hm and hm[req_val]!=i:
-                return [i,hm[req_val]]
-
+        hm_nums = {val:i for i, val in enumerate(nums)}
+        for i,val in enumerate(nums):
+            a = val
+            if (target - a in hm_nums and i != hm_nums[target - a]):
+                return [i, hm_nums[target - a]]
         
+        return [-1, -1]        
 
-
-
-# hm={2:0,7:1,11:2,15:3}
-# nums=[2,7,11,15]
-#       i
-# req_val =9-2 =7
-# target-nums[i]=7
-# hm[7]=1
-              
-# target=9
-
-# [3,2,4] target =6
-#  i
-# hm[3:0,2:1,4:2]
-
-# i!=hm[req] and target-nums[i] in hm
-
-
-        
+# [2,7,11,15]
+#  i 
+#    j 
+# a = 2
+# j = 7
+          
