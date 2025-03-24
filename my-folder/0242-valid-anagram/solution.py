f878e55f5c1b837e6b95a61493ad07ff5c2801e8
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-24 20:07:40 +0000

    Sync LeetCode submission Runtime - 7 ms (89.40%), Memory - 17.9 MB (64.17%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,5 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count1 = Counter(s)
+        count2 = Counter(t)
+        for i in set(s):
+            if count1[i] != count2[i]:
+                return False
+        return True
+
         # s_freqs={}
         # t_freqs={}
         # for i in s:
