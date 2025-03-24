a2811a526b95cefba0f71d79aee0c79577c02657
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-24 03:57:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (61.56%)

--- my-folder/0021-merge-two-sorted-lists/solution.py
+++ my-folder/0021-merge-two-sorted-lists/solution.py
@@ -1,24 +1,40 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        node=newLL=ListNode()
-        while list1 and list2:
-            if list1.val>list2.val:
-                newLL.next=list2 #assign the newll's next variable to list2 node
-                list2=list2.next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        ll = ListNode(0)
+        new_list = ll
+        temp1 = list1
+        temp2 = list2
+        while temp1 and temp2:
+            if(temp1.val > temp2.val):
+                ll.next = temp2
+                temp2 = temp2.next
             else:
-                newLL.next=list1 #else assign it to list1's node
-                list1=list1.next
-            newLL=newLL.next # for every case move newLL pointing to nextvariable
-        newLL.next= list1 or list2 #add the rest of list1 or list2 to the newLL
-        return node.next#firstnode of the newLL 
+                ll.next = temp1
+                temp1 = temp1.next
+            
+            ll = ll.next
+        
+        if temp1:
+            ll.next = temp1
 
+        if temp2:
+            ll.next = temp2
+        
+        return new_list.next
+
+
+            
+
+# 1-> 2 -> 4
+#     h
+# 1 -> 3   -> 4
+# h
+
+# ll - 1 ->
+        
+        
