068fb5e2250e1266f6b8988df4ad164066545051
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-25 01:48:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.8 MB (37.52%)

--- my-folder/0206-reverse-linked-list/solution.py
+++ my-folder/0206-reverse-linked-list/solution.py
@@ -1,21 +1,18 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    
-    def reverseList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        curr,prev=head,None
-        while curr:
-            tmp=curr.next #store next address of the curr
-            curr.next=prev #changing the direction of link
-            prev=curr #set prev to curr's value
-            curr=tmp #set curr to with the next value of curr
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = head
+        prev = None
+        while temp:
+            curr = temp
+            temp = temp.next
+            curr.next = prev
+            prev = curr
+        
         return prev
-
+        
         
