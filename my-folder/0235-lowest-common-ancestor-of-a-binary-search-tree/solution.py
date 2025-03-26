ef022de9b1225036a77237f1d48bbb59da2b9208
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-26 20:47:12 +0000

    Sync LeetCode submission Runtime - 71 ms (5.39%), Memory - 21.3 MB (5.42%)

--- my-folder/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
+++ my-folder/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
@@ -1,24 +1,24 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        curr=root
-        while curr:
-            if curr.val<p.val and curr.val<q.val:
-                curr=curr.right
-            elif curr.val>p.val and curr.val>q.val:
-                curr=curr.left
-            else:
-                return curr
-        
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root.val == p.val or root.val == q.val:
+            return root
+
+        if root.val < p.val and root.val < q.val: 
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif q.val < root.val and p.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
+
+# [6,2,8,0,4,7,9,null,null,3,5]
+# p = 2
+# q = 4
+# root = 6
+
