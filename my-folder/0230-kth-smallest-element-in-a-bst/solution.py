95015f76eeb27517869f227cb73575616b79417b
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-27 01:12:46 +0000

    Sync LeetCode submission Runtime - 4 ms (15.19%), Memory - 21.1 MB (27.50%)

--- my-folder/0230-kth-smallest-element-in-a-bst/solution.py
+++ my-folder/0230-kth-smallest-element-in-a-bst/solution.py
@@ -6,20 +6,18 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        curr=root
-        stack=[]
-        c=0
-        while stack or curr:
-            while curr:
-                stack.append(curr)
-                curr=curr.left
-            curr=stack.pop()
-            c+=1
-            if(c==k):
-                return curr.val
-            curr=curr.right
-
-
+        # store k in a temp variable
+        self.arr = []
+        self.dfs(root)
+        print(self.arr)
+        return self.arr[k - 1]
+        
+    def dfs(self, node):
+        if not node:
+            return None
+        self.dfs(node.left)
+        self.arr.append(node.val)
+        self.dfs(node.right)
 
 
         
