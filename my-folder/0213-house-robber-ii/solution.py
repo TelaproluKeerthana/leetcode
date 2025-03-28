5b92f3ed6c63e35f79ba1a82caaf8a06cb5c4e23
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-28 00:05:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (86.55%)

--- my-folder/0213-house-robber-ii/solution.py
+++ my-folder/0213-house-robber-ii/solution.py
@@ -1,11 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        return max(nums[0],self.helper(nums[1:]),self.helper(nums[:-1]))
-        #considering the case where there is only one element then it will be the max value and it will be retured.
-    def helper(self,nums):
-        rob1,rob2=0,0
-        for n in nums:
-            tmp=max(rob1+n,rob2)
-            rob1=rob2
-            rob2=tmp
-        return rob2
+    
+        if len(nums) == 1:
+            return nums[0]
+        
+        if len(nums) == 2:
+            return max(nums)
+        
+        return max(self.robb(nums[1:]) , self.robb(nums[:-1]))
+
+    def robb(self, arr):
+        prev = 0 
+        curr = 0
+        for i in arr:
+            temp = curr
+            curr = max(curr, i + prev)
+            prev = temp
+    
+        return curr
+
+
+
+
+
+
+
+        
