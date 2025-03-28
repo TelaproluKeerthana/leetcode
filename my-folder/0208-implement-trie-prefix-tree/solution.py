394335ae17a638e598730e751a13378358a3b8a8
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-28 22:56:58 +0000

    Sync LeetCode submission Runtime - 56 ms (36.51%), Memory - 32.3 MB (42.17%)

--- my-folder/0208-implement-trie-prefix-tree/solution.py
+++ my-folder/0208-implement-trie-prefix-tree/solution.py
@@ -1,17 +1,41 @@
-class Trie:
-   
+class TrieNode:
     def __init__(self):
-        self.HM={}
-    def insert(self, word: str) -> None:
-            self.HM[word]=1
-    def search(self, word: str) -> bool:
-        return word in self.HM
+        self.children = {}
+        self.end = False
+class Trie:
 
-    def startsWith(self, prefix: str) -> bool:
-        for word in self.HM.keys():
-            if word.startswith(prefix):
-                return True
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for w in word:
+            if w not in curr.children:
+                curr.children[w] = TrieNode()
+            curr = curr.children[w]
+        
+        curr.end = True
+        
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for w in word:
+            if w not in curr.children:
+                return False
+            curr = curr.children[w]
+        if curr.end == True:
+            return True
+        
         return False
+        
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for char in prefix:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        
+        return True
         
 
 
