00161169637d4e95330bad6a11d3d137b2a9e1d6
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-29 03:20:57 +0000

    Sync LeetCode submission Runtime - 3 ms (71.46%), Memory - 18.6 MB (17.56%)

--- my-folder/0073-set-matrix-zeroes/solution.py
+++ my-folder/0073-set-matrix-zeroes/solution.py
@@ -3,20 +3,30 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        visit = set()
+        row_flag = False
+        col_flag = False
         for row in range(len(matrix)):
             for col in range(len(matrix[0])):
                 if matrix[row][col] == 0:
-                    visit.add((row,col))
+                    if row == 0:
+                        row_flag = True
+                    if col == 0:
+                        col_flag = True
+                    elif row != 0 and col != 0:
+                        matrix[row][0] = 0
+                        matrix[0][col] = 0
         
+        for row in range(1,len(matrix)):
+            for col in range(1, len(matrix[0])):
+                if matrix[row][0] == 0 or matrix[0][col] == 0:
+                    matrix[row][col] = 0
+
+        if row_flag:
+            matrix[0] = [0] * len(matrix[0])
         
-        for i in range(len(matrix)):
-            for row,col in visit:
-                matrix[i][col] = 0
-        
-        for j in range(len(matrix[0])):
-            for row,col in visit:
-                matrix[row][j] = 0
+        if col_flag:
+            for row in range(len(matrix)):
+                matrix[row][0] = 0
             
 
 
