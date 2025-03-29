a2bea38be30f4c7f779ed28bd4ab99121aa002ce
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-29 21:51:37 +0000

    Sync LeetCode submission Runtime - 5133 ms (23.96%), Memory - 17.9 MB (39.62%)

--- my-folder/0079-word-search/solution.py
+++ my-folder/0079-word-search/solution.py
@@ -1,27 +1,45 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows=len(board)
-        cols=len(board[0])
-        path=set()
-        def dfs(r,c,i):
-            #if we didn't return false until the length of the word and we moved to next element then we obviously found the element
-            if(i==len(word)):
+        visit = set()
+        def dfs(r, c, i):
+            if i == len(word):
                 return True
-            # if r and c dont go out of bounds and the path is not yet covered and the word is not equal
-            if r>=rows or c>=cols or c<0 or r<0 or board[r][c]!=word[i] or (r,c) in path:
+            
+            if (r < 0 or c < 0 or r >= len(board) or c >= len(board[0]) or (r,c) in visit or word[i] != board[r][c]):
                 return False
-            #as we move in a path append the path direction to the path
-            path.add((r,c)) 
-            #if we found a path from any of those recursive calls we return true or false
-            res=(dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c-1,i+1) or dfs(r,c+1,i+1))
-            path.remove((r,c))
+
+            visit.add((r,c))
+            res = dfs(r + 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c - 1, i + 1)
+            visit.remove((r,c))
             return res
-        for r in range(rows):
-            for c in range(cols):
-                if dfs(r,c,0):
+        
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if dfs(r, c, 0):
                     return True
+        
         return False
 
 
+# visit = (0,0), (0, 1) (0,2) (1, 2) 
+# dfs(0, 0, 0)
+# res = false or dfs(0, 1, 1) 
+#                dfs(0, 2, 2) - dfs(1,2,3) - dfs()
+                                        
+
+
+                              
+                                
+
+
+
+
+
+
+
+
+    
+
+        
 
         
