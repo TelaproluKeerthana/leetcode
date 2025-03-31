8f61597f72dd08b2784d302b47471e031248c809
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-31 02:52:34 +0000

    Sync LeetCode submission Runtime - 232 ms (86.66%), Memory - 20.1 MB (82.34%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -1,25 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        total_islands=0
-        visit=set()
-        rows=len(grid)
-        cols=len(grid[0])
-        def dfs(r,c):
-            if(r<0 or c<0 or r>=rows or c>=cols or grid[r][c]=="0" or (r,c) in visit):
+        rows = len(grid)
+        cols = len(grid[0])
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
                 return 
-            visit.add((r,c))
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
 
+            grid[r][c] = "0"
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c - 1)
+            dfs(r, c + 1)
+            
+        components = 0
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]=="1" and (r,c) not in visit:
-                    dfs(r,c)
-                    total_islands += 1
-        return total_islands
-
-
+                if(grid[r][c] == "1"):
+                    dfs(r, c)
+                    components += 1
+        
+        return components
             
             
+
