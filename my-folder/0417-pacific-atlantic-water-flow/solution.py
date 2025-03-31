f93f7c9b916e428c04b09773924125eea143ccad
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-03-31 01:15:05 +0000

    Sync LeetCode submission Runtime - 39 ms (30.91%), Memory - 19.2 MB (90.58%)

--- my-folder/0417-pacific-atlantic-water-flow/solution.py
+++ my-folder/0417-pacific-atlantic-water-flow/solution.py
@@ -1,28 +1,35 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows=len(heights)
-        cols=len(heights[0])
-        atl=set()
-        pac=set()
-        def dfs(r,c,visit,prevHeight):
-            if(min(r,c)<0 or r>=rows or c>=cols or (r,c) in visit or heights[r][c] < prevHeight):
-                return 
+        rows = len(heights)
+        cols = len(heights[0])
+        pacific = set()
+        atlantic = set()
+
+        def dfs(r, c, visit, prevHeight):
+            if (r, c) in visit or r < 0 or c < 0 or r >= rows or c >= cols or heights[r][c] <  prevHeight:
+                return
+
+            visit.add((r, c))
             
-            visit.add((r,c))
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
+            currHeight = heights[r][c]
+            dfs(r + 1, c, visit, currHeight)
+            dfs(r - 1, c, visit, currHeight)
+            dfs(r, c + 1, visit, currHeight)
+            dfs(r, c - 1, visit, currHeight)
+
+        for col in range(cols):
+            dfs(0, col, pacific, heights[0][col])
+            dfs(rows - 1, col, atlantic, heights[rows - 1][col])
         
-        for r in range(rows):
-            dfs(r,0,pac,heights[r][0])
-            dfs(r,cols-1,atl,heights[r][cols-1])
-        for c in range(cols):
-            dfs(rows-1,c,atl,heights[rows-1][c])
-            dfs(0,c,pac,heights[0][c])
-        res=[]
+        for row in range(rows):
+            dfs(row, 0, pacific, heights[row][0])
+            dfs(row, cols - 1, atlantic, heights[row][cols - 1])
+        res = []
+
         for r in range(rows):
             for c in range(cols):
-                if (r,c) in pac and (r,c) in atl:
+                if (r,c) in pacific and (r,c) in atlantic:
                     res.append([r,c])
         return res
+
+        
