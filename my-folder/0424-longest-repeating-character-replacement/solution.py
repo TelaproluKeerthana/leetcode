ff851239b815e9e8fe705866d86d59d2c044cb5c
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-04-01 19:43:15 +0000

    Sync LeetCode submission Runtime - 111 ms (84.28%), Memory - 18 MB (86.76%)

--- my-folder/0424-longest-repeating-character-replacement/solution.py
+++ my-folder/0424-longest-repeating-character-replacement/solution.py
@@ -1,17 +1,26 @@
+from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        #counter hashmap to store frequencies
-        count={}
-        L=0
-        res=0
-        maxf=0
+        L = 0  
+        counter = defaultdict(int)
+        longest_substring = 0
+        maxFreq = 0
         for R in range(len(s)):
-            count[s[R]]=1+count.get(s[R],0)
-            maxf=max(maxf,count[s[R]])
-            while((R-L+1)-maxf>k):
-                count[s[L]]-=1
-                L+=1
-            res=max(res,R-L+1)
-        return res
+            counter[s[R]] += 1
+            maxFreq = max(maxFreq, counter[s[R]])
+            while(R - L + 1 - maxFreq > k):
+                counter[s[L]] -= 1
+                L += 1
+            longest_substring = max(R - L + 1, longest_substring)
+      
+        return longest_substring
+
+        
+
+            
+            
+
+            
 
 
+        
