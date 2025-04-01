32c37c5071050844dd6ff9a89583ac625ed898a3
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-04-01 16:56:17 +0000

    Sync LeetCode submission Runtime - 20 ms (25.60%), Memory - 22.9 MB (11.56%)

--- my-folder/0049-group-anagrams/solution.py
+++ my-folder/0049-group-anagrams/solution.py
@@ -2,19 +2,13 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hm = defaultdict(list)
         for word in strs:
-            key = "".join(sorted(word))
-            hm[key].append(word)
-        
-        res = []
-        for i in hm.values():
-            res.append(i)
-
-        return res
-
-
-        
+            key = [0] * 26
+            for char in word:
+                key[ord(char) - ord('a')] += 1
             
-                
-
+            hm[tuple(key)].append(word)
+        
+        return [i for i in hm.values()]
+        
 
         
