8436581ee9c04eed8655101821559258084cae64
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-04-07 02:14:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (54.33%)

--- my-folder/0075-sort-colors/solution.py
+++ my-folder/0075-sort-colors/solution.py
@@ -3,28 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # dutch national flag algorithm 
-        freq_counter = [0] * 3
-        for i in range(len(nums)):
-            freq_counter[nums[i]] += 1
-        
-        idx = 0
-        for curr in range(len(freq_counter)):
-            while(freq_counter[curr] > 0 and idx < len(nums)):
-                nums[idx] = curr
-                idx += 1
-                freq_counter[curr] -= 1
-
-        print(nums)
-            
-        
-
-
-            
-        
-
-            
-
+        red = 0
+        white = 0
+        blue = len(nums) - 1
+        #red pushes all the numbers greater than 0 to the right
+        #blue moves ahead after setting all the 2's at the end of the array
+        while white <= blue:
+            if nums[white] == 0:
+                nums[white], nums[red] = nums[red], nums[white]
+                red += 1
+                white += 1
+            # only when the nums[white] is 1 then increment white this can be case where blue and white came to same position and we almost sorted  the array
+            elif nums[white] == 1:
+                white += 1 
+            # when the nums[white] is 2 then we swap with blue decr blue 
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
+                
 # [2,0,2,1,1,0]
 #  l r
 
