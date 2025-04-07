0b498b9300c2c2441e119607869c89e32e054d8d
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-04-07 21:58:43 +0000

    Sync LeetCode submission Runtime - 35 ms (75.83%), Memory - 20.4 MB (95.33%)

---
+++ my-folder/0475-heaters/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
+        i = 0
+        res =  0
+        for house in houses:
+            while (i < len(heaters) - 1 and abs(heaters[i + 1] - house) <= abs(heaters[i] - house)):
+                i += 1
+        
+            res = max(res, abs(heaters[i] - house))
+        
+        return res
+
+
+        
+
+
+#houses = [1,2,3]
+#heaters = [2]
+
