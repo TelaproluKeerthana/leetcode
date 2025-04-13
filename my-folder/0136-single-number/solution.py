6d5acca54e5f3b71409d11ec9ae7ca980ec77bff
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-04-13 22:46:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (93.88%)

---
+++ my-folder/0136-single-number/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        single = nums[0]
+        for i in range(1, len(nums)):
+            single ^= nums[i]
+        
+        return single
+            
+
+        
