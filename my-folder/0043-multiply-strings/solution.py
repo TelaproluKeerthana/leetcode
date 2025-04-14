2f21db905a164351d63c6d49c0d4fa4c45c502f2
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-04-14 22:54:08 +0000

    Sync LeetCode submission Runtime - 59 ms (34.08%), Memory - 17.7 MB (71.14%)

---
+++ my-folder/0043-multiply-strings/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        L = len(num1) - 1
+        R = len(num2) - 1
+        res = 0
+        carry1 = 1
+        for i in num1[::-1]:
+            carry2 = 1
+            for j in num2[::-1]:
+                res += int(i ) * int(j) * carry1 * carry2
+                carry2 *= 10
+            
+            carry1 *= 10
+        
+        return str(res)
+
+
+
+
+        
