8bb8b2a15c57cfc414e2115656a39db4e747dfd5
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-05-20 21:23:15 +0000

    Sync LeetCode submission Runtime - 2 ms (98.57%), Memory - 43.1 MB (74.93%)

--- my-folder/0125-valid-palindrome/solution.java
+++ my-folder/0125-valid-palindrome/solution.java
@@ -1,14 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        char[] array = s.toCharArray();
-        for(char c:array){
-            if(Character.isLetterOrDigit(c)){
-                sb.append(Character.toLowerCase(c));
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right --;
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left ++;
+            }
+
+            if(Character.toLowerCase(s.charAt(left))  != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+
+            left ++;
+            right --;            
         }
-    }
-    String filtered = sb.toString();
-    String reverseFiltered = sb.reverse().toString();
-    return filtered.equals(reverseFiltered);
+
+        return true;
 }
 }
