cee432920224fa06f8760b9e57b12107482b0fb4
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-05-26 18:08:54 +0000

    Sync LeetCode submission Runtime - 4 ms (28.04%), Memory - 44.7 MB (47.06%)

--- my-folder/0036-valid-sudoku/solution.java
+++ my-folder/0036-valid-sudoku/solution.java
@@ -1,20 +1,63 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-         HashSet<String> seen = new HashSet<>();
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char number = board[i][j];
-                if (number != '.') {
-                    if (!seen.add(number + " in row " + i) ||
-                        !seen.add(number + " in col " + j) ||
-                        !seen.add(number + " in block " + i / 3 + "-" + j / 3)) {
-                        return false;
-                    }
+        Map<Integer, HashSet<Character>> rowHash = new HashMap<>();
+        Map<Integer, HashSet<Character>> colHash = new HashMap<>();
+        Map<Integer, HashSet<Character>> subGridHash = new HashMap<>();
+        int rows = board.length;
+        int cols = board[0].length;
+        
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(board[i][j] == '.') continue;
+                
+                if(!rowHash.containsKey(i)){
+                    rowHash.put(i, new HashSet<Character>());
                 }
+
+                if(rowHash.get(i).contains(board[i][j])){
+                    return false;
+                }
+
+                rowHash.get(i).add(board[i][j]);
+
+                if(!colHash.containsKey(j))
+                {
+                    colHash.put(j, new HashSet<Character>());
+                } 
+
+                if(colHash.get(j).contains(board[i][j])){
+                    return false;
+                }
+                
+                colHash.get(j).add(board[i][j]);
+
+                int key = getSubGridKey(i, j);
+                if(!subGridHash.containsKey(key))
+                {
+                    subGridHash.put(key, new HashSet<Character>());
+                } 
+
+                if(subGridHash.get(key).contains(board[i][j])){
+                    return false;
+                }
+                
+                subGridHash.get(key).add(board[i][j]);
             }
         }
+
         return true;
+    } 
+    
+    private int getSubGridKey(int row, int col){
+        if(row/3 == 0){
+            return col/3;
+        }
+        else if(row/3 == 1){
+            return 3 + col/3;
+        }
         
+        return 6 + col/3;  
     }
 }
+
+
