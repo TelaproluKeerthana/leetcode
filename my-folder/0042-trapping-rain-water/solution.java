596887a8254a60431f2bead88e37a8e34655eac5
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-06-01 16:09:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.4 MB (54.92%)

--- my-folder/0042-trapping-rain-water/solution.java
+++ my-folder/0042-trapping-rain-water/solution.java
@@ -1,23 +1,26 @@
 class Solution {
     public int trap(int[] height) {
-        int length = height.length;
-        int[] prefMax = new int[length];
-        int[] suffMax = new int[length];
-
-        prefMax[0] = height[0];
-        for(int i = 1; i < length; i++){
-            prefMax[i] = Math.max(height[i], prefMax[i - 1]);
-        }
-
-        suffMax[length - 1] = height[length - 1];
-        for(int i = length - 2; i >= 0; i--){
-            suffMax[i] = Math.max(height[i], suffMax[i + 1]);
-        }
-        System.out.println(Arrays.toString(prefMax));
-        System.out.println(Arrays.toString(suffMax));
-        int waterTrapped = 0;
-        for(int i = 0; i < length; i++){
-            waterTrapped += Math.abs(Math.min(prefMax[i], suffMax[i]) - height[i]);
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0, waterTrapped = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= leftMax){
+                    leftMax = height[left];
+                }
+                else{
+                    waterTrapped += leftMax - height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right] >= rightMax){
+                    rightMax = height[right];
+                }
+                else{
+                    waterTrapped += rightMax - height[right];
+                }
+                right--;
+            }  
         }
 
         return waterTrapped;
