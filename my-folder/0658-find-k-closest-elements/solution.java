4632c4bfb6dc631b9fb035ad38865fa71d2042ac
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-06-07 23:18:03 +0000

    Sync LeetCode submission Runtime - 4 ms (96.73%), Memory - 46.1 MB (53.13%)

--- my-folder/0658-find-k-closest-elements/solution.java
+++ my-folder/0658-find-k-closest-elements/solution.java
@@ -1,57 +1,23 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int[] diffArr = new int[arr.length];
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> {
-            int diffA = Math.abs(a - x);
-            int diffB = Math.abs(b - x);
-            if(diffA == diffB) return Integer.compare(a, b);
-            return Integer.compare(diffA, diffB);
-        });
-        
-        for(int num : arr){
-            minHeap.offer(num);
+        int left = 0;
+        // fixing right 
+        int right = arr.length - k;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(x - arr[mid] > arr[mid + k] - x){
+                left = mid + 1;
+            }
+            else{
+                right = mid;
+            }
         }
 
         List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            result.add(minHeap.poll());
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
         }
 
-        Collections.sort(result);
         return result;
-
     }
 }
-
-
-// arr =  [1,2,3,4,5]
-// k = 4 
-//     - 4 closest integers to 3
-
-// x = 3
-// for an integer to be closer to x it should be |a - x| < |b - x|
-// 1- 3 < 2 - 3 
-// 2 < 1
-// 1 - 3 == 5 - 3 and 1 < 5
-//  so 1 is closer to 3 
-
-// 3 - 4 < 3- 5 and 4 < 5
-// so 4 is closer 
-
-// 2 - 3 == 4 - 3
-// so 2 is closer
-
-// 3 itself is closer so we add 3 
-
-// 1, 2, 3, 4
-
-// 1 : 3 - 1 = 2
-// 2 :3 - 2 = 1
-// 3 : 3 - 3 = 0
-// 4 : 3 - 4 = 1
-// 5 : 3 - 5 = 5
-
-// now add them to the list in sorted order and return it if theres a tie we need to return the number which is smallest
-
-// now i need to get minimum difference and the idx if not i need to get min difference is same for multiple indexes then we get the one which is less than the other
-
