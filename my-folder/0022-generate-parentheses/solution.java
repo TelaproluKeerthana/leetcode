94904de46646b649ca81ca6bb3a0052a7476949f
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-06-16 15:14:50 +0000

    Sync LeetCode submission Runtime - 2 ms (44.31%), Memory - 43.3 MB (47.58%)

--- my-folder/0022-generate-parentheses/solution.java
+++ my-folder/0022-generate-parentheses/solution.java
@@ -1,30 +1,21 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        getAll("",0, n, result);
+        getAll("",0, 0, n, result);
         return result;
     }
 
-    private void getAll(String curr, int index, int n, List<String> result){
-        if(curr.length() == n * 2){
-            if(isValid(curr)){
-                result.add(curr);
-            }
+    private void getAll(String curr, int open, int close, int max, List<String> result){
+        if(curr.length() == max * 2){
+            result.add(curr);
             return;
         }
-
-        getAll(curr + "(", index + 1, n, result);
-        getAll(curr + ")", index + 1, n, result);
-    }
-
-    private boolean isValid(String curr){
-        int balanced = 0;
-        for(char c:curr.toCharArray()){
-            if(c == '(') balanced += 1;
-            else balanced -= 1; 
-            if(balanced < 0) return false;
+        if(open < max){
+            getAll(curr + "(", open + 1, close, max, result);
         }
-        return balanced == 0;
+        if(close < open){
+            getAll(curr + ")", open, close + 1, max, result);
+        }
     }
 
 
