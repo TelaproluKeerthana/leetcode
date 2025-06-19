a4a8d6c823ec205658939a7c633286e1cce1ee20
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-06-19 18:41:38 +0000

    Sync LeetCode submission Runtime - 4 ms (93.45%), Memory - 45.4 MB (55.81%)

--- my-folder/0735-asteroid-collision/solution.java
+++ my-folder/0735-asteroid-collision/solution.java
@@ -1,38 +1,39 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
+        // picking a datastructure can be anything, your choice can use stack or deque or even with an array
         Deque<Integer> stack = new ArrayDeque<>();
         for(int asteroid:asteroids){
             boolean destroyed = false;
-            while(!stack.isEmpty() && asteroid < 0 && stack.peekLast() > 0){
+            while(!stack.isEmpty() && stack.peekLast() > 0 && 0 > asteroid){
                 int top = stack.peekLast();
                 if(top < -asteroid){
+                    // when the asteroid is heavier than the top the top of stack explodes
                     stack.pollLast();
                 }
                 else if(top == -asteroid){
+                   // when the top and asteroid are travelling towards each other and are of same size
                     stack.pollLast();
                     destroyed = true;
                     break;
                 }
                 else{
+                    // when the asteroid is smaller than the top of the stack and is negative that will be destroyed, so update flag so you dont append it to the stack
                     destroyed = true;
                     break;
                 }
-            } 
+            }
 
             if(!destroyed){
                 stack.offerLast(asteroid);
-            }  
+            }
         }
 
         int[] result = new int[stack.size()];
         int i = 0;
-        for (int val : stack) {
+        for(int val:stack){
             result[i++] = val;
         }
+
         return result;
-    
     }
 }
-// only when stack is empty then add negative ones to stack else check top of stack and if less dont insert if greater pop element from stack.
-// if greater keep the element and check the next top of the stack if that is also small pop do it until either of it pops 
-
