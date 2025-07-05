dabd26f5730a2c09ce0e8b4a937caef30f94c1a2
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-07-05 03:22:39 +0000

    Sync LeetCode submission Runtime - 175 ms (5.40%), Memory - 125 MB (5.13%)

--- my-folder/1023-time-based-key-value-store/solution.java
+++ my-folder/1023-time-based-key-value-store/solution.java
@@ -1,12 +1,12 @@
 class TimeMap {
 
-    Map<String, TreeMap<Integer, String>> store;
+    Map<String, List<String[]>> store;
     public TimeMap() {
     store = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        store.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
+        store.computeIfAbsent(key, k -> new ArrayList<>()).add(new String[]{String.valueOf(timestamp), value});
     }
     
     public String get(String key, int timestamp) {
@@ -14,12 +14,23 @@ class TimeMap {
         return "";
      }
      
-     TreeMap<Integer, String> tree = store.get(key);
-     Integer floorKey = tree.floorKey(timestamp);  
-     if(floorKey == null){
-        return "";
-     } 
-     return tree.get(floorKey);
+     List<String[]> list = store.get(key); 
+     int left = 0;
+     int right = list.size() - 1;
+     int res = -1;
+     while(left <= right){
+        int mid = left + (right - left) / 2;
+        int currMid = Integer.parseInt(list.get(mid)[0]);
+        if(currMid <= timestamp){
+            res = mid;
+            left = mid + 1;
+        }
+        else{
+            right = mid - 1;
+        }
+     }
+
+     return res == -1 ? "" : list.get(res)[1];
     }
 }
 
