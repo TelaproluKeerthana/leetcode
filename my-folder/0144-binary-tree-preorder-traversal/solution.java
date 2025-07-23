28cdc256b66c24951dbc16bc85e08b2fb903ec89
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-07-23 23:03:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.9 MB (18.76%)

--- my-folder/0144-binary-tree-preorder-traversal/solution.java
+++ my-folder/0144-binary-tree-preorder-traversal/solution.java
@@ -15,17 +15,23 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        dfs(root, result);
-        return result;        
-    }
-
-    private void dfs(TreeNode node, List<Integer> result){
-            if(node == null){
-                return;
-            }
-            result.add(node.val);
-            dfs(node.left, result);
-            dfs(node.right, result);
+       List<Integer> result = new ArrayList<>();
+       if(root == null){
+            return result;
         }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode curr = stack.pop();
+            result.add(curr.val);
+            if(curr.right != null){
+                stack.push(curr.right);
+            }
+           if(curr.left != null){
+            stack.push(curr.left);
+           }
+        }
+
+        return result;
+}
 }
