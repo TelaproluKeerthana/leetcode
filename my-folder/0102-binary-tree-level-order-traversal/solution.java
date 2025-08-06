e04eab92fc76275bcdc188b3549af6fc3f5e48a1
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-08-06 00:05:11 +0000

    Sync LeetCode submission Runtime - 1 ms (89.59%), Memory - 45 MB (57.57%)

--- my-folder/0102-binary-tree-level-order-traversal/solution.java
+++ my-folder/0102-binary-tree-level-order-traversal/solution.java
@@ -1,33 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if (root == null) {
-            return result; // return empty list if root is null
+        if(root == null){
+            return result;
         }
-
         Queue<TreeNode> lot = new ArrayDeque<>();
         lot.offer(root);
-
-        while (!lot.isEmpty()) {
+        while(!lot.isEmpty()){
             int length = lot.size();
-            List<Integer> currentLevel = new ArrayList<>();
-
-            for (int i = 0; i < length; i++) {
+            List<Integer> currLevel = new ArrayList<>();
+            for(int i = 0; i < length; i++){
                 TreeNode curr = lot.poll();
-                currentLevel.add(curr.val);
-
-                if (curr.left != null) {
+                currLevel.add(curr.val);
+                if(curr.left != null){
                     lot.offer(curr.left);
                 }
-                if (curr.right != null) {
+                if(curr.right != null){
                     lot.offer(curr.right);
                 }
             }
-
-            result.add(currentLevel);
+            result.add(currLevel);
         }
 
         return result;
     }
 }
-
