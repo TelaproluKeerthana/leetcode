2814109a8442f8766faeec115700692af5aad0db
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-08-08 23:45:55 +0000

    Sync LeetCode submission Runtime - 20 ms (84.37%), Memory - 49.8 MB (95.67%)

---
+++ my-folder/0789-kth-largest-element-in-a-stream/solution.java
@@ -0,0 +1,25 @@
+class KthLargest {
+    Queue<Integer> minHeap;
+    int cap;
+    public KthLargest(int k, int[] nums) {
+        minHeap = new PriorityQueue<>();
+        cap = k;
+        for(int num : nums){
+            add(num);
+        }
+    }
+    
+    public int add(int val) {
+        minHeap.add(val);
+        if(minHeap.size() > cap){
+            minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
