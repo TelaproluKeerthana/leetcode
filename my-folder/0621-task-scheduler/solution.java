ec5b53683bd65d18d75d93fec1e6d2722fc3ad66
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-08-18 23:56:15 +0000

    Sync LeetCode submission Runtime - 23 ms (50.00%), Memory - 45.9 MB (53.92%)

---
+++ my-folder/0621-task-scheduler/solution.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        // array to keep track of the count of tasks
+        int[] count = new int[26];
+        for (char task : tasks) {
+            count[task - 'A']++;
+        }
+    
+        // push only the counts to maxheap
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int cnt : count) {
+            if (cnt > 0) {
+                maxHeap.add(cnt);
+            }
+        }
+        
+        int time = 0;
+        Queue<int[]> q = new LinkedList<>();
+        while (!maxHeap.isEmpty() || !q.isEmpty()) {
+            time++;
+
+            if (maxHeap.isEmpty()) {
+                time = q.peek()[1];
+            } else {
+                int cnt = maxHeap.poll() - 1;
+                if (cnt > 0) {
+                    q.add(new int[]{cnt, time + n});
+                }
+            }
+
+            if (!q.isEmpty() && q.peek()[1] == time) {
+                maxHeap.add(q.poll()[0]);
+            }
+        }
+
+        return time;
+    }
+}
