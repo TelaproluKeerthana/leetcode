4a85bc5f541f8cafce9841b727496461495ca58a
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-08-29 23:05:30 +0000

    Sync LeetCode submission Runtime - 109 ms (33.44%), Memory - 80.2 MB (80.98%)

--- my-folder/1962-single-threaded-cpu/solution.java
+++ my-folder/1962-single-threaded-cpu/solution.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] getOrder(int[][] tasks) {
     int len = tasks.length;
+    //we store all those tasks with indexing
     int[][] indexedTasks = new int[len][3];
     // indexed all the tasks, now we know which tasks arrive at what time
     for(int i = 0; i < len; i++){
@@ -12,21 +13,30 @@ class Solution {
     // sorted according to process starting time
     Arrays.sort(indexedTasks, (a, b) -> Integer.compare(a[0], b[0]));
 
-    PriorityQueue<int[]> cpu = new PriorityQueue<>((a, b) -> a[1] == b[1] ? Integer.compare(a[2], b[2]) : Integer.compare(a[1], b[1]));
+    PriorityQueue<int[]> cpu = new PriorityQueue<>((a, b) -> {if(a[1] == b[1]){
+        return Integer.compare(a[2], b[2]);
+    } else{ return Integer.compare(a[1], b[1]);}
+    }
+    );
 
     int result[] = new int[len];
     
-    // total time it is taking
+    // the current time
     int time = 0;
+    // the current index we're processing
     int idx = 0;
+    // the index of the result array
     int resIdx = 0;
-
+    
+    // when the cpu is not empty or the index is in bounds 
     while(idx < len || !cpu.isEmpty()){
+        // we keep all the indexedtasks into the priorityQueue
         while(idx < len && indexedTasks[idx][0] <= time){
             cpu.offer(indexedTasks[idx]);
             idx++;
         }
-
+       
+       // when no tasks are available in priorityQueue, we process the next available task 
         if(cpu.isEmpty()){
             time = indexedTasks[idx][0];
         }
@@ -36,8 +46,12 @@ class Solution {
             time += task[1];
         }
     }
-
     return result;
+}
+}
 
-}
-}
+
+// [[1,2],[2,4],[3,2],[4,1]]
+
+// indexed = [[1,2, 0],[2,4, 1],[3,2, 2],[4,1, 3]]
+// priority queue = []
