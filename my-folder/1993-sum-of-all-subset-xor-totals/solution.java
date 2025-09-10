edd45cf7541dbbd1517697916b273872802f9d73
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-09-10 21:07:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.1 MB (57.05%)

--- my-folder/1993-sum-of-all-subset-xor-totals/solution.java
+++ my-folder/1993-sum-of-all-subset-xor-totals/solution.java
@@ -1,29 +1,15 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-    List<List<Integer>> subsets = new ArrayList<>(); 
-    getSubSets(nums, 0, new ArrayList<>(), subsets);
-    int result = 0;
-    for(List<Integer> subset : subsets){
-        int subsetXORTotal  = 0;
-        for(int num: subset){
-            subsetXORTotal ^= num;
-        }
-        result += subsetXORTotal;
+        return xorSum(nums, 0, 0);
     }
-    return result;
-    }
-    private void getSubSets(int[] nums, int index, List<Integer> subset, List<List<Integer>> subsets){
-         if (index == nums.length) {
-            subsets.add(new ArrayList<>(subset));
-            return;
+
+    private int xorSum(int[] nums, int index, int currXOR){
+        if(index == nums.length){
+            return currXOR;
         }
 
-        subset.add(nums[index]);
-        getSubSets(nums, index + 1, subset, subsets);
-        subset.remove(subset.size() - 1);
-        
-        getSubSets(nums, index + 1, subset, subsets);
+        int withElement = xorSum(nums, index + 1, currXOR ^ nums[index]);
+        int withoutElement = xorSum(nums, index + 1, currXOR);
+        return withElement + withoutElement;
     }
 }
-
-
