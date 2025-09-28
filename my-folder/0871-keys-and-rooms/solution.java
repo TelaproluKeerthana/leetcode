194796239b5f0aa3d89051065cc0214e3b3f1349
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-09-28 03:44:22 +0000

    Sync LeetCode submission Runtime - 2 ms (52.28%), Memory - 44 MB (95.29%)

--- my-folder/0871-keys-and-rooms/solution.java
+++ my-folder/0871-keys-and-rooms/solution.java
@@ -1,35 +1,25 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        // build a adjacency list 
-        // Map<Integer, List<Integer>> adjList = new HashMap<>();
-        // for(int i = 0; i < rooms.size(); i++){
-        //     adjList.put(i, rooms.get(i));
-        // // }
+        int n = rooms.size();
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new ArrayDeque<>();
 
-        Set<Integer> visited = new HashSet<>();
-        Queue<Integer> que = new ArrayDeque<>();
-        que.offer(0);
-        visited.add(0);
-        while(!que.isEmpty()){
-            int curr = que.poll();
-            List<Integer> currRoomNeighbors = rooms.get(curr);
-            for(int i = 0; i < currRoomNeighbors.size(); i++){
-                int neighbor = currRoomNeighbors.get(i);
-                if(!visited.contains(neighbor)){
-                    que.offer(neighbor);
-                    visited.add(neighbor);
+        queue.offer(0);
+        visited[0] = true;
+        int visitedCount = 1;
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            for (int neighbor : rooms.get(curr)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    visitedCount++;
+                    queue.offer(neighbor);
                 }
             }
         }
 
-        return visited.size() == rooms.size();
-
-
+        return visitedCount == n;
     }
 }
 
-// {"0" : 1,
-// "1" : 2,
-// "2" : 3,
-// "3" : ""
-// }
