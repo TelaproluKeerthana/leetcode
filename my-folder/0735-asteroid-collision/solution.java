d0b6c74e73247a1f654b308f1d959a280e796365
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-10-15 13:24:12 +0000

    Sync LeetCode submission Runtime - 7 ms (40.16%), Memory - 45.6 MB (37.13%)

--- my-folder/0735-asteroid-collision/solution.java
+++ my-folder/0735-asteroid-collision/solution.java
@@ -1,39 +1,41 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        // picking a datastructure can be anything, your choice can use stack or deque or even with an array
-        Deque<Integer> stack = new ArrayDeque<>();
-        for(int asteroid:asteroids){
-            boolean destroyed = false;
-            while(!stack.isEmpty() && stack.peekLast() > 0 && 0 > asteroid){
-                int top = stack.peekLast();
-                if(top < -asteroid){
-                    // when the asteroid is heavier than the top the top of stack explodes
-                    stack.pollLast();
-                }
-                else if(top == -asteroid){
-                   // when the top and asteroid are travelling towards each other and are of same size
-                    stack.pollLast();
-                    destroyed = true;
+        Stack<Integer> stack = new Stack<>();
+        for(int asteroid : asteroids){
+             boolean destroy = false;
+             // what cases causes destruction
+             while(!stack.isEmpty() && asteroid < 0 && stack.peek() > 0){
+                if(stack.peek() == -asteroid){
+                    stack.pop();
+                    destroy = true;
                     break;
+                }
+                else if(stack.peek() < -asteroid){
+                    stack.pop();
+                    continue;
                 }
                 else{
-                    // when the asteroid is smaller than the top of the stack and is negative that will be destroyed, so update flag so you dont append it to the stack
-                    destroyed = true;
+                    destroy = true;
                     break;
                 }
-            }
+             }
 
-            if(!destroyed){
-                stack.offerLast(asteroid);
-            }
+             if(!destroy){
+                stack.push(asteroid);
+             } 
         }
 
-        int[] result = new int[stack.size()];
-        int i = 0;
-        for(int val:stack){
-            result[i++] = val;
+        int[] res = new int[stack.size()];
+        int i = stack.size() - 1;
+        while(i >= 0){
+            res[i] = stack.pop();
+            i--;
         }
 
-        return result;
+        return res;
     }
 }
+
+// asteroids = [5,10,-5]
+// check tos = 5 
+// stack = [5]
