6a8e9ec8f6d899e3606855be974f877ea76c3a7e
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-10-26 01:08:41 +0000

    Sync LeetCode submission Runtime - 8 ms (89.67%), Memory - 45.1 MB (86.80%)

--- my-folder/2576-minimum-penalty-for-a-shop/solution.java
+++ my-folder/2576-minimum-penalty-for-a-shop/solution.java
@@ -1,54 +1,19 @@
 class Solution {
     public int bestClosingTime(String customers) {
-        int n = customers.length();
-        int[] yays = new int[n + 1];
-        int[] nays = new int[n + 1];
-        int idx = 1;
-        if(customers.charAt(0) == 'Y'){
-            yays[0] = 1;
-            nays[0] = 0;
-        }
-        else{
-            nays[0] = 1;
-            yays[0] = 0;
-        }
-        // for (int i = 1; i <= n; i++) {
-        // char c = customers.charAt(i - 1);        
-        // yays[i] = yays[i - 1];
-        // nays[i] = nays[i - 1];
+        int score = 0;
+        int maxScore = 0;
+        int bestTime = 0;
+        for(int i = 0; i < customers.length(); i++){
+            char curr = customers.charAt(i);
+            if(curr == 'Y') score++; 
+            else score--;
 
-        // if (c == 'Y') {
-        //     yays[i]++;
-        // } else {
-        //     nays[i]++;
-        // }
-        // }
-        for (int i = 1; i <= n; i++) {
-            char c = customers.charAt(i - 1);
-            yays[i] = yays[i - 1] + (c == 'Y' ? 1 : 0);
-            nays[i] = nays[i - 1] + (c == 'N' ? 1 : 0);
+            if(score > maxScore){
+                maxScore = score;
+                bestTime = i + 1;
+            }  
         }
 
-        int minPenality = Integer.MAX_VALUE;
-        int bestHour = 0;
-
-        for(int i = 0; i <= n; i++){
-                int penality = nays[i] + (yays[n] - yays[i]);
-                if(penality < minPenality){
-                    minPenality = penality;
-                    bestHour = i;
-                }
-        }
-    return bestHour;
+        return bestTime;
+    }
 }
-}
-
-
-
-
-// y = [1 2 2 3]
-// n = [0 0 1 0]
-
-//      1  2 1 3
-
-
