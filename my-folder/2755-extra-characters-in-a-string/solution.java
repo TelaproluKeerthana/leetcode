ffe32a578c2750c6f127d7c2c6a2400228301ea1
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-10-30 16:56:41 +0000

    Sync LeetCode submission Runtime - 16 ms (76.90%), Memory - 46.1 MB (6.14%)

--- my-folder/2755-extra-characters-in-a-string/solution.java
+++ my-folder/2755-extra-characters-in-a-string/solution.java
@@ -1,22 +1,62 @@
+class TrieNode{
+    Map<Character, TrieNode> children = new HashMap<>();
+    boolean isEnd = false;
+}
+
+class Trie{
+    TrieNode root;
+    public Trie()
+    {
+        root = new TrieNode();
+    }
+
+    void addword(String word){
+        TrieNode curr = root;
+        for(char w : word.toCharArray()){
+            if(!curr.children.containsKey(w)){
+                curr.children.put(w, new TrieNode());
+            }
+            curr = curr.children.get(w);
+        }
+        curr.isEnd = true;
+    }
+}
+
 class Solution {
     public int minExtraChar(String s, String[] dictionary) {
         // storing words in hashset for easier lookups
-        Set<String> dict = new HashSet<>(Arrays.asList(dictionary));
+        // Set<String> dict = new HashSet<>(Arrays.asList(dictionary));
+        Trie trie = new Trie();
+        for(String word:dictionary){
+            trie.addword(word);
+        }
+
         Map<Integer, Integer> dp = new HashMap<>();
         dp.put(s.length(), 0);
-        return  dfs(0, s, dp, dict);
+        return  dfs(0, s, dp, trie.root);
     }
 
-    private int dfs(int idx, String s, Map<Integer, Integer> dp, Set<String> dict){
+    private int dfs(int idx, String s, Map<Integer, Integer> dp, TrieNode root){
         if(dp.containsKey(idx)){
             return dp.get(idx);
         }
-
-        int res = 1 + dfs(idx + 1, s, dp, dict);
+        TrieNode curr = root;
+        int res = 1 + dfs(idx + 1, s, dp, curr);
         for(int j = idx; j < s.length(); j++){
-            if(dict.contains(s.substring(idx, j + 1))){
-                res = Math.min(res, dfs(j + 1, s, dp, dict));
+            // if(dict.contains(s.substring(idx, j + 1))){
+            //     res = Math.min(res, dfs(j + 1, s, dp, dict));
+            // }
+            char c = s.charAt(j);
+            if(!root.children.containsKey(c)){
+                break;
             }
+
+            root = root.children.get(c);
+            if(root.isEnd){
+                res = Math.min(res, dfs(j + 1, s, dp, curr));
+            }
+            
+            
         }
         dp.put(idx, res);
         return res;
