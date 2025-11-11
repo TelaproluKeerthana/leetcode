ba0ab015099c14e3028cfaa4ab39e9480affa910
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-11-11 14:25:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (5.64%)

--- my-folder/0070-climbing-stairs/solution.java
+++ my-folder/0070-climbing-stairs/solution.java
@@ -3,13 +3,50 @@ class Solution {
         if(n == 1 || n == 2){
             return n;
         }
-        int prev = 1;
         int curr = 2;
-        for (int i = 3; i <= n; i++){
+        int prev = 1;
+        for(int i = 3; i <= n; i++){
             int temp = curr;
-            curr = prev + curr;
+            curr = curr + prev;
             prev = temp;
         }
-        return curr;
+    
+    return curr;
     }
 }
+
+// // n = 4
+// curr = 2
+// prev = 1
+// ---
+// curr = 2 + 1
+// prev = 2
+
+// -----
+// curr = 3
+// prev = 2
+
+// -----
+
+// 4  curr = 5
+//    prev = 3
+// 1 1 1 1
+// 1 2 1
+// 1 1 2
+// 2 2 
+// 2 1 1
+
+// 5 curr 8 prev 5
+// 1 1 1 1 1
+// 1 2 1 1 
+// 1 1 1 2
+// 2 1 1 1
+// 1 1 2 1
+// 2 2 1
+// 2 1 2
+// 2 2 1
+
+
+
+
+
