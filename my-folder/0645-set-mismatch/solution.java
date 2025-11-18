21a718168bcf7c64e357144db948122106df647a
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-11-18 17:35:19 +0000

    Sync LeetCode submission Runtime - 2 ms (93.42%), Memory - 47.7 MB (22.27%)

--- my-folder/0645-set-mismatch/solution.java
+++ my-folder/0645-set-mismatch/solution.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        Map<Integer, Integer> cntMap = new HashMap<>();
-        for(int num : nums){
-            cntMap.put(num, cntMap.getOrDefault(num , 0) + 1);
-        }
-        int[] res = new int[2];
         int n = nums.length;
+        int[] res = new int[2];
+        int[] freq = new int[n + 1];
+        for(int i = 0; i < n; i++){
+            freq[nums[i]] += 1;
+        }
+
         for(int i = 1; i <= n; i++){
-            if(cntMap.getOrDefault(i, 0) == 0){
+            if(freq[i] == 0){
                 res[1] = i;
             }
-            else if(cntMap.get(i) == 2){
-                res[0] = i;  
+            else if(freq[i] == 2){
+                res[0] = i;
             }
         }
 
