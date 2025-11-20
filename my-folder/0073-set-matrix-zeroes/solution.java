8a75f90c91b2afc3bfaacec2d283a6307a5b1218
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-11-20 16:30:59 +0000

    Sync LeetCode submission Runtime - 1 ms (65.10%), Memory - 47.2 MB (22.61%)

--- my-folder/0073-set-matrix-zeroes/solution.java
+++ my-folder/0073-set-matrix-zeroes/solution.java
@@ -2,34 +2,27 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
-
-        boolean firstColZero = false;
-
-        // 1. Use first row and first column as markers
-        for (int r = 0; r < rows; r++) {
-
-            if (matrix[r][0] == 0) {
-                firstColZero = true;
+        boolean col0 = false;
+        for(int r = 0; r < rows; r++){
+            if(matrix[r][0] == 0){
+                col0 = true;
             }
-
-            for (int c = 1; c < cols; c++) {
-                if (matrix[r][c] == 0) {
-                    matrix[r][0] = 0; 
-                    matrix[0][c] = 0; 
-                }
+            for(int c = 1; c < cols; c++){
+                if(matrix[r][c] == 0){
+                    matrix[0][c] = 0;
+                    matrix[r][0] = 0;
+                }  
             }
         }
 
-        // 2. Iterate from bottom-right to avoid overwriting markers
-        for (int r = rows - 1; r >= 0; r--) {
-            for (int c = cols - 1; c >= 1; c--) {
-                if (matrix[r][0] == 0 || matrix[0][c] == 0) {
+        for(int r = rows - 1; r >= 0; r--){
+            for(int c = cols - 1; c >= 1; c--){
+                if(matrix[0][c] == 0 || matrix[r][0] == 0){
                     matrix[r][c] = 0;
                 }
             }
-
-            if (firstColZero) {
-                matrix[r][0] = 0;
+            if(col0){
+                    matrix[r][0] = 0;
             }
         }
     }
