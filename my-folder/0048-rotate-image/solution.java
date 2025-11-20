25cc2a6785c1449b903e284bef2da3dfb2f12168
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-11-20 20:22:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.5 MB (28.71%)

--- my-folder/0048-rotate-image/solution.java
+++ my-folder/0048-rotate-image/solution.java
@@ -43,3 +43,17 @@ class Solution {
 // 7 4 1
 // 8 5 2
 // 9 6 3
+
+// [5, 1, 9, 11]
+// [2, 4, 8, 10]
+// [13, 3, 6,  7]
+// [15, 14, 12,16]
+// 9 - 2 - 14 - 7
+// -----
+// copy 5 replace 5 with 15, 15 with 16, 16 with 11 and 11 with the copied 5
+// copy 1 replace 1 with 13, 13 with 12, 12 with 10, 10 with 1
+// copy 9, replace 9 with 2, 2 with 14, 14 with 7,  7 with copied 9
+// ---------
+// copy 4 replace 4 with 3, replace 3 with 6, 6 with 8, 8 with copied 4
+// --- 
+
