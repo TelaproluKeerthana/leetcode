3833f466f7f501bcdfb5c6b0af5a0a9f43fe5f56
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-11-22 18:09:24 +0000

    Sync LeetCode submission Runtime - 17 ms (53.65%), Memory - 43.5 MB (54.75%)

--- my-folder/0005-longest-palindromic-substring/solution.java
+++ my-folder/0005-longest-palindromic-substring/solution.java
@@ -1,41 +1,39 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int maxResLen = 0;
-        int startPtr = 0;
-        int endPtr = 0;
+        int maxlen = 0, startPtr = 0, endPtr = 0;
         for(int i = 0; i < s.length(); i++){
-            // for odd palindromes
             int left = i;
             int right = i;
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){    
-               int reslen = right - left + 1;
-
-                if(maxResLen < reslen){
-                    maxResLen = reslen;
+            while(left  >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                if(maxlen < right - left + 1){
+                    maxlen = right - left + 1;
                     startPtr = left;
                     endPtr = right;
                 }
-
-                left -= 1;
-                right += 1;
+                left--;
+                right++;
             }
 
             left = i;
             right = i + 1;
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-                int reslen = right - left + 1;
-                if(maxResLen < reslen){
-                    maxResLen = reslen;
+            while(left  >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                if(maxlen < right - left + 1){
+                    maxlen = right - left + 1;
                     startPtr = left;
                     endPtr = right;
                 }
-                left -= 1;
-                right += 1;
+                left--;
+                right++;
             }
-            // for even palindromes
-
         }
 
         return s.substring(startPtr, endPtr + 1);
     }
 }
+// maxlen = 0,
+// startPtr = 0,
+// endPtr = 0;
+// // babad
+//    i
+//    l
+//    r
