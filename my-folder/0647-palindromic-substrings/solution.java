c8d6d50f32babac7e963e67c07cad8741a6b004a
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-11-22 19:27:25 +0000

    Sync LeetCode submission Runtime - 6 ms (66.17%), Memory - 42.7 MB (51.41%)

--- my-folder/0647-palindromic-substrings/solution.java
+++ my-folder/0647-palindromic-substrings/solution.java
@@ -1,43 +1,24 @@
 class Solution {
     public int countSubstrings(String s) {
-        List<String> substrings = generateSubStrings(s);
-        int subs = 0;
-        for(int idx = 0; idx < substrings.size(); idx++){
-            if(ispalindrome(substrings.get(idx))){
-                subs += 1;
-            }
-        }
-        return subs;
-    }
-
-    private List<String> generateSubStrings(String s){
-        List<String> res = new ArrayList<>();
+        int count = 0;
         for(int i = 0; i < s.length(); i++){
-            for(int j = i; j < s.length(); j++){
-                res.add(s.substring(i, j + 1));
+            int left = i;
+            int right = i;
+            while(left >= 0 && right  < s.length() && s.charAt(left) == s.charAt(right)){
+                count++;
+                left--;
+                right++;
             }
-        }
 
-        return res;
-    }
-
-    private boolean ispalindrome(String s){
-        int right = s.length() - 1;
-        int left = 0;
-        while(left < right){
-            if(s.charAt(left) != s.charAt(right)){
-                return false;
+            left = i;
+            right = i + 1;
+            while(left >= 0 && right  < s.length() && s.charAt(left) == s.charAt(right)){
+                count++;
+                left--;
+                right++;
             }
-            left++;
-            right--;
         }
         
-        return true;
+        return count;
     }
 }
-// a ab abc b bc c
-// // abc
-//     i
-//      j
-//    a, b, c, ab, bc, ca, abc
-
