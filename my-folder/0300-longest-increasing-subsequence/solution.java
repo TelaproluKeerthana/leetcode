e31fec0a8bafe1899fa23c8f8ffd8f2a3ca03cba
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-11-23 04:20:01 +0000

    Sync LeetCode submission Runtime - 41 ms (54.27%), Memory - 46.6 MB (22.60%)

--- my-folder/0300-longest-increasing-subsequence/solution.java
+++ my-folder/0300-longest-increasing-subsequence/solution.java
@@ -13,3 +13,17 @@ public class Solution {
         return Arrays.stream(LIS).max().getAsInt();
     }
 }
+// dp[4]
+// 3 
+// max(1, 3) = 3
+// max(3, 1)
+//   7 101 18
+// dp[3]
+// 5,
+//   3,7,101,18
+// dp[3] = 1 + dp[5] = 4
+//       = 1 + dp[6] = 4 its max
+
+// dp = [2 2 4  3 3 2 1 1]
+// max(dp) = 4
+
