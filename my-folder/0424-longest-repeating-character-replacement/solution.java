d71ef5137e5d818a3776063b678c6cadcd875e50
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-11-25 16:45:12 +0000

    Sync LeetCode submission Runtime - 25 ms (33.79%), Memory - 47.2 MB (6.89%)

--- my-folder/0424-longest-repeating-character-replacement/solution.java
+++ my-folder/0424-longest-repeating-character-replacement/solution.java
@@ -1,24 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        Map<Character, Integer> counter = new HashMap<>();
-        int longestSubstrLength = 0;
-        int left = 0;
+        Map<Character, Integer> cntMap = new HashMap<>();
         int maxFreq = 0;
+        int left = 0;
+        int lcr = 0;
         for(int right = 0; right < s.length(); right++){
             char curr = s.charAt(right);
-            counter.put(curr, counter.getOrDefault(curr, 0) + 1);
-            maxFreq = Math.max(maxFreq, counter.get(curr));
-            // for every right value update the maxfreq and check if we can get a longest substr and update it
-            while( (right - left + 1) - maxFreq > k){
-                char leftChar = s.charAt(left);
-                counter.put(leftChar, counter.get(leftChar) - 1);
-                // variable size sliding window we shift the window until where we can make only k replacements and form a substring
-                left++; 
+            cntMap.put(curr, cntMap.getOrDefault(curr, 0) + 1);
+            maxFreq = Math.max(maxFreq, cntMap.get(curr));
+            while(right - left + 1 - maxFreq > k){
+                char prev = s.charAt(left);
+                cntMap.put(prev, cntMap.get(prev) - 1);
+                left++;
             }
-
-            longestSubstrLength = Math.max(longestSubstrLength, right - left + 1);
+            lcr = Math.max(right - left + 1, lcr);
         }
-  
-    return longestSubstrLength;
+        return lcr;
     }
 }
+// window = {a : 2, b : 2} maxfreq = 2; 4 - 2 update lcr = to window length and return it
+
+// ABAB 
+// k = 2
