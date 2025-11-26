ddf350293336fe55d2483928968d370643f9d4ed
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-11-26 18:40:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.3 MB (40.34%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.java
+++ my-folder/0033-search-in-rotated-sorted-array/solution.java
@@ -1,31 +1,55 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while(left <= right){
-            int mid = (left + right) / 2;
-            if(nums[mid] == target){
-                return mid;
-            } 
-            if (nums[left] <= nums[mid])
-            {
-                if(nums[left] > target || nums[mid] < target){
-                    left = mid + 1;
-                }  
-                else{
-                    right = mid  - 1;
-                }
-            }
-            else{
-                if(nums[right] < target || nums[mid] > target){
-                    right = mid - 1;
-                }
-                else{
-                    left = mid + 1;
-                }
-            }
+     int left = 0;
+     int right = nums.length - 1;
+     while(left <= right){
+        int mid = left + (right - left) / 2;
+        if(nums[mid] == target){
+            return mid;
         }
         
-        return -1;
+        if(nums[left] <= nums[mid]){
+            if(target < nums[left] || nums[mid] < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        else{
+            if(nums[mid] > target || target > nums[right]){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+
+    } 
+        return -1;  
     }
 }
+
+    // l
+    //       m
+    //             r
+// [4,5,6,7,0,1,2]
+// mid = 3(7)
+// 4 < 7 and target > nums[left] (4) :
+//     left = mid + 1 [4]
+
+
+// left = 4 right = 7 11/2 = 5
+// nums[left] < nums[mid] && target < nums[left]:
+//     search in right half
+// else
+//     shrink right to the mid
+
+// nums[left] = 4 nums[right] = 5
+//     mid = 4
+//     if(nums[mid] == target):
+//         return mid
+
+// target = 0
+
+
