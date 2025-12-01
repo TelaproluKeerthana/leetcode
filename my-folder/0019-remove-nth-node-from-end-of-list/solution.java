a41edb6708ca916a6f2c8ba056ec48ee1d6acda6
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-12-01 22:32:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.4 MB (28.04%)

--- my-folder/0019-remove-nth-node-from-end-of-list/solution.java
+++ my-folder/0019-remove-nth-node-from-end-of-list/solution.java
@@ -32,3 +32,24 @@ class Solution {
         return dummy.next;
     }
 }
+// one approach
+// get length by iterating over the list and then iterate till n - k - 1 steps and move pointer to the next's next node.
+
+// other approach 
+// 1 -> 2 -> 3 -> 4 -> 5
+// you want to remove the kth node from the end 
+// so take slow fast ptrs and move the fast ptr n steps ahead to the slow ptr
+// 1 -> 2 -> 3 -> 4 -> 5
+//           f
+// s
+// n = 2, move f two steps and decr n till 0
+// move fast until fast becomes null, move slow and fast till that point
+//                          f
+// 1 -> 2 -> 3 -> 5
+//                (break link between 3 and 4 and point it to 5) 
+//           s
+
+// slow.next = slow.next.next
+// s (break link connecting to 2)
+// 1 -> 2
+//     f
