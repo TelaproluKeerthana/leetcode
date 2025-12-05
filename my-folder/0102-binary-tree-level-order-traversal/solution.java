8788c4fad0b90071772cf1a17b0d1d46bbc67062
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-12-05 16:40:08 +0000

    Sync LeetCode submission Runtime - 1 ms (91.54%), Memory - 46.8 MB (31.29%)

--- my-folder/0102-binary-tree-level-order-traversal/solution.java
+++ my-folder/0102-binary-tree-level-order-traversal/solution.java
@@ -15,28 +15,30 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         if(root == null){
-            return result;
-        }
-        Queue<TreeNode> lot = new ArrayDeque<>();
-        lot.offer(root);
-        while(!lot.isEmpty()){
-            int length = lot.size();
-            List<Integer> currLevel = new ArrayList<>();
-            for(int i = 0; i < length; i++){
-                TreeNode curr = lot.poll();
-                currLevel.add(curr.val);
-                if(curr.left != null){
-                    lot.offer(curr.left);
-                }
-                if(curr.right != null){
-                    lot.offer(curr.right);
-                }
-            }
-            result.add(currLevel);
+            return res;
         }
 
-        return result;
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(root);
+
+        while(!que.isEmpty()){
+            List<Integer> curr = new ArrayList<>();
+            int size = que.size();
+            for(int i = 0; i < size; i++){
+                TreeNode currnode = que.poll();
+                curr.add(currnode.val);
+                if(currnode.left != null){
+                    que.offer(currnode.left);
+                }
+                if(currnode.right != null){
+                    que.offer(currnode.right);
+                }
+            }
+            res.add(curr);
+        }
+
+        return res;
     }
 }
