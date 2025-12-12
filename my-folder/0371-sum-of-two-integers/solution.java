7dff3248cc37c3b566d62eacd0b2bb5050837fe5
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-12-12 21:20:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (15.11%)

--- my-folder/0371-sum-of-two-integers/solution.java
+++ my-folder/0371-sum-of-two-integers/solution.java
@@ -1,10 +1,23 @@
 class Solution {
     public int getSum(int a, int b) {
         while(b != 0){
-            int temp = (a & b) << 1;
+            int temp = (a & b) << 1; // for carry overs
             a = a ^ b;
-            b = temp; 
+            b = temp;
         }
         return a;
     }
 }
+
+// 0001
+// 0010
+// ----
+
+// 0010
+// 0011
+// ----
+// 0010 << 1 = 0100 
+        //     0001 
+        //     ----
+        //     0101
+
