6c5dc9b7f7979305dba9e6a8fc67b00a431d2873
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-12-17 17:46:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (30.12%)

--- my-folder/0066-plus-one/solution.java
+++ my-folder/0066-plus-one/solution.java
@@ -1,30 +1,36 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-       int carry = 1;
-       int n = digits.length;
-       List<Integer> lis = new ArrayList<>();
-       for(int idx = n - 1; idx >= 0; idx--){
-            int sum = carry + digits[idx];
-            carry = sum / 10;
-            lis.add(sum % 10);
-       } 
+        List<Integer> alist = new ArrayList<>();
+        int carry = 1;
+        for(int i = digits.length - 1; i >= 0; i--){
+            int currSum = carry + digits[i];
+            carry = currSum / 10;
+            alist.add(currSum % 10);
+        }
 
-       if(carry == 1){
-        lis.add(1);
-       }
+        if(carry == 1){
+            alist.add(1);
+        } 
+        int[] res = new int[alist.size()];
+        int idx = 0;
+        for(int i = alist.size() - 1; i >= 0; i--){
+            res[idx++] = alist.get(i);
+        }
 
-       // reverse the list
-       int[]  res = new int[lis.size()];
-       int siz = lis.size() - 1;
-       for(int i = 0; i < lis.size(); i++){
-        res[i] = lis.get(siz--);
-       }
-
-       return res;
+        return res;
     }
 }
 
-    // 1 2 3
-    // lis = 4 2
+// 123
+ 
+//  currIdx = len - 1
+//  lastelement = digits[len - 1]
+//  if(lastelem == 9){
+//     lastelem = 0
+//     currIdx = currIdx - 1
+//  }
 
-         
+// currSum = 1 + 3
+// 123
+//   i
+
