0a818bf2fc4423a705aba7dca7972d5d85fa4a5c
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-12-19 22:41:35 +0000

    Sync LeetCode submission Runtime - 1 ms (99.27%), Memory - 47.4 MB (22.84%)

--- my-folder/0045-jump-game-ii/solution.java
+++ my-folder/0045-jump-game-ii/solution.java
@@ -1,35 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        if (n == 1) return 0;         
-        int goal = n - 1;
-        Queue<Integer> que = new LinkedList<>();
-        que.offer(0);
+        int left = 0; 
+        int right = 0; 
         int steps = 0;
-        boolean[] visited = new boolean[goal + 1];
-        visited[0] = true;
-        while(!que.isEmpty()){
-            int size = que.size();
-            steps++;
-            while (size-- > 0) {
-                int curr = que.poll();
-                int maxReach = curr + nums[curr];
+        while(right < nums.length - 1){
+            int maxtravel = 0;
+           for(int i = left; i < right + 1; i++){
+                maxtravel = Math.max(maxtravel, i + nums[i]);
+           }
 
-                if (maxReach >= goal) return steps;
-
-                for (int next = curr + 1; next <= maxReach; next++) {
-                    if (!visited[next]) {
-                        visited[next] = true;
-                        que.offer(next);
-                    }
-                }
-            }
+           left = right + 1;
+           right = maxtravel;
+           steps += 1;
         }
 
-        return 0;
+        return steps;
     }
 }
-
-// [2,3,1,1,4]
-    // max(3 + 1, 1 + 2)
-
