b2b60fcc3318206f15d36e768d125a35a53470ed
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-12-22 16:10:13 +0000

    Sync LeetCode submission Runtime - 30 ms (71.39%), Memory - 156.2 MB (12.92%)

--- my-folder/0239-sliding-window-maximum/solution.java
+++ my-folder/0239-sliding-window-maximum/solution.java
@@ -1,32 +1,33 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        List<Integer> res = new ArrayList<>();
-        int right = 0;
-        Queue<Integer> window = new LinkedList<>();
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        while(right < nums.length){
-            int curr = nums[right++];
-            window.offer(curr);
-            map.put(curr, map.getOrDefault(curr, 0) + 1);
-
-            if(k == window.size()){
-                res.add(map.lastKey());
-                int removeKey = window.poll();
-                map.put(removeKey, map.get(removeKey) - 1);
-                if(map.get(removeKey) == 0){
-                    map.remove(removeKey);
-                }
+        int n = nums.length;
+        int[] output = new int[n - k + 1];
+        Deque<Integer> q = new LinkedList<>();
+        int l = 0, r = 0;
+        
+        // you're building a monotonically decreasing queue of indices, such that you have the smallest at end and max at beginning 
+        while(r < n){
+            // when current element is greater than the last element. remove all elements until the statement is false
+            while(!q.isEmpty() && nums[q.getLast()] < nums[r]){
+                q.removeLast();
             }
+            // then keep the current indices at the right position
+            q.addLast(r);
+
+            //when the current max is not within curr window range, remove it
+            if(l > q.getFirst()){
+                q.removeFirst();
+            }
+
+            //when curr r is greater than the window size, pick the current max for that window
+            if((r + 1) >= k){
+                output[l] = nums[q.getFirst()];
+                l++;
+            }
+
+            r++;
         }
 
-        int[] result = new int[res.size()];
-        for(int i = 0; i < res.size(); i++){
-            result[i] = res.get(i);
-        }
-
-        return result;
+        return output;
     }
 }
-// currMax = 3
-// 1 3 -1 -3
-// [1,3,-1,-3,5,3,6,7]
