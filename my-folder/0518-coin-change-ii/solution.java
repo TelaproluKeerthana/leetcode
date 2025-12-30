c15c7afc0be7f730afdb91903b4f284b6b1115e4
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2025-12-30 21:11:50 +0000

    Sync LeetCode submission Runtime - 5 ms (99.75%), Memory - 43 MB (84.77%)

--- my-folder/0518-coin-change-ii/solution.java
+++ my-folder/0518-coin-change-ii/solution.java
@@ -1,36 +1,13 @@
 class Solution {
-    int memo[][];
     public int change(int amount, int[] coins) {
-        Arrays.sort(coins);
-        memo = new int[coins.length + 1][amount + 1];
-        for(int[] row : memo){
-            Arrays.fill(row, -1);
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for(int coin : coins){
+            for(int a = coin; a <= amount; a++){
+                dp[a] += dp[a - coin];
+            }
         }
 
-        return dfs(0, amount, coins);
-    }
-
-    public int dfs(int idx, int amount, int[] coins){
-        if(amount == 0){
-            return 1;
-        }
-
-        if(idx >= coins.length){
-            return 0;
-        }
-
-        if(memo[idx][amount] != -1){
-            return memo[idx][amount];
-        }
-        
-        int res = 0;
-        if(amount >= coins[idx]){
-            res = dfs(idx + 1, amount, coins);
-            res += dfs(idx, amount - coins[idx], coins);
-        }
-
-        memo[idx][amount] = res;
-        return memo[idx][amount];
+        return dp[amount];
     }
 }
-
