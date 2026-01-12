b2eeede1162958dce97115d55094108a1e57231f
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-01-12 22:12:40 +0000

    Sync LeetCode submission Runtime - 1 ms (99.54%), Memory - 48.9 MB (40.28%)

--- my-folder/0004-median-of-two-sorted-arrays/solution.java
+++ my-folder/0004-median-of-two-sorted-arrays/solution.java
@@ -1,33 +1,43 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int[] merged = new int[n + m];
+        int[] arr1 = nums1;
+        int[] arr2 = nums2;
+        int total = nums1.length + nums2.length;
+        int half = (total + 1) / 2;
 
-        int fp = 0, sp = 0, i = 0;
+        if(nums1.length > nums2.length){
+            int[] temp = nums1;
+            arr1 = arr2;
+            arr2 = temp;
+        }
 
-        while (fp < n && sp < m) {
-            if (nums1[fp] <= nums2[sp]) {
-                merged[i++] = nums1[fp++];
-            } else {
-                merged[i++] = nums2[sp++];
+        int left = 0;
+        int right = arr1.length;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            int mid2 = half - mid;
+
+            int aleft = mid > 0 ? arr1[mid - 1] : Integer.MIN_VALUE;
+            int aright = mid < arr1.length ? arr1[mid] : Integer.MAX_VALUE;
+            int bleft = mid2 > 0 ? arr2[mid2 - 1] : Integer.MIN_VALUE;
+            int bright = mid2 < arr2.length ? arr2[mid2] : Integer.MAX_VALUE;
+            
+            // this is the case when we have elements less than the a1 end in b array
+            if(aleft <= bright && bleft <= aright){
+                if(total % 2 != 0){
+                    return Math.max(aleft, bleft);
+                }
+
+                return (Math.max(aleft, bleft) + Math.min(aright, bright))/ 2.0;
+            }
+            else if(aleft > bright){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
             }
         }
 
-        while (fp < n) {
-            merged[i++] = nums1[fp++];
-        }
-
-        while (sp < m) {
-            merged[i++] = nums2[sp++];
-        }
-
-        int len = n + m;
-        if (len % 2 == 1) {
-            return merged[len / 2];
-        } else {
-            return (merged[len / 2 - 1] + merged[len / 2]) / 2.0;
-        }
+        return -1;
     }
 }
-
