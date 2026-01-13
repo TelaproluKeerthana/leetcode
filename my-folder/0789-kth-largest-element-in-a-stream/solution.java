3572160f04b3159db5f4c1996bc1121ac5f2ae86
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-01-13 23:59:36 +0000

    Sync LeetCode submission Runtime - 2767 ms (5.16%), Memory - 57.9 MB (6.16%)

--- my-folder/0789-kth-largest-element-in-a-stream/solution.java
+++ my-folder/0789-kth-largest-element-in-a-stream/solution.java
@@ -1,25 +1,19 @@
 class KthLargest {
-    Queue<Integer> minHeap;
-    int cap;
+    List<Integer> aList = new ArrayList<>();
+    int k;
+
     public KthLargest(int k, int[] nums) {
-        minHeap = new PriorityQueue<>();
-        cap = k;
-        for(int num : nums){
-            add(num);
+        this.k = k;                 // store k
+        for (int num : nums) {
+            aList.add(num);
         }
+        Collections.sort(aList);   // sort once initially
     }
     
     public int add(int val) {
-        minHeap.add(val);
-        if(minHeap.size() > cap){
-            minHeap.poll();
-        }
-        return minHeap.peek();
+        aList.add(val);            // use correct variable name
+        Collections.sort(aList);  // keep sorted
+        return aList.get(aList.size() - k);  // kth largest
     }
 }
 
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest obj = new KthLargest(k, nums);
- * int param_1 = obj.add(val);
- */
