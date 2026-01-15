f242f9c565950145280a69d8a3ad5b9c8fb23904
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-01-15 23:29:21 +0000

    Sync LeetCode submission Runtime - 1 ms (99.54%), Memory - 49.1 MB (9.01%)

--- my-folder/0004-median-of-two-sorted-arrays/solution.java
+++ my-folder/0004-median-of-two-sorted-arrays/solution.java
@@ -1,12 +1,11 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] arr1 = nums1;
-        int[] arr2 = nums2;
-        int total = nums1.length + nums2.length;
+    public double findMedianSortedArrays(int[] num1, int[] num2) {
+        int total = num1.length + num2.length;
         int half = (total + 1) / 2;
-
-        if(nums1.length > nums2.length){
-            int[] temp = nums1;
+        int[] arr1 = num1;
+        int[] arr2 = num2;
+        if(num1.length > num2.length){
+            int[] temp = num1;
             arr1 = arr2;
             arr2 = temp;
         }
@@ -16,19 +15,17 @@ class Solution {
         while(left <= right){
             int mid = (left + right) / 2;
             int mid2 = half - mid;
-
             int aleft = mid > 0 ? arr1[mid - 1] : Integer.MIN_VALUE;
             int aright = mid < arr1.length ? arr1[mid] : Integer.MAX_VALUE;
             int bleft = mid2 > 0 ? arr2[mid2 - 1] : Integer.MIN_VALUE;
             int bright = mid2 < arr2.length ? arr2[mid2] : Integer.MAX_VALUE;
-            
-            // this is the case when we have elements less than the a1 end in b array
+
             if(aleft <= bright && bleft <= aright){
                 if(total % 2 != 0){
                     return Math.max(aleft, bleft);
                 }
 
-                return (Math.max(aleft, bleft) + Math.min(aright, bright))/ 2.0;
+                return (Math.max(aleft, bleft) + Math.min(bright, aright)) / 2.0;
             }
             else if(aleft > bright){
                 right = mid - 1;
