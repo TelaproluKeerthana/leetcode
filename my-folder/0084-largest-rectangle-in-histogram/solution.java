fa9d9077ba84a4534f5c9d1fdcc3257968826bbd
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-01-21 23:26:54 +0000

    Sync LeetCode submission Runtime - 106 ms (29.19%), Memory - 77.9 MB (65.62%)

--- my-folder/0084-largest-rectangle-in-histogram/solution.java
+++ my-folder/0084-largest-rectangle-in-histogram/solution.java
@@ -1,57 +1,54 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-         int n = heights.length;
-        int[] leftMost = new int[n];
-        int[] rightMost = new int[n];
+        int len = heights.length;
+        int[] lheights = new int[len];
+        int[] rheights = new int[len];
         Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i < n; i++){
-            leftMost[i] = -1;
-            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
-                stack.pop();
+        
+        // prefix array checking the min 
+        for(int i = 0; i < len; i++){
+            int curr = heights[i];
+            lheights[i] = -1;
+            while(!stack.isEmpty() && heights[stack.peek()] >= curr){
+                stack.pop();               
             }
-
             if(!stack.isEmpty()){
-                leftMost[i] = stack.peek();
+                lheights[i] = stack.peek();
             }
-
             stack.push(i);
         }
-
+        
         stack.clear();
 
-        for(int i = n - 1; i >= 0; i--){
-            rightMost[i] = n;
-            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+        for(int i = len - 1; i >= 0; i--){
+            int curr = heights[i];
+            rheights[i] = len;
+            while(!stack.isEmpty() && heights[stack.peek()] >= curr){
                 stack.pop();
             }
-
-            if(!stack.isEmpty()){
-                rightMost[i] = stack.peek();
+             if(!stack.isEmpty()){
+                rheights[i] = stack.peek();
             }
-
             stack.push(i);
+
         }
 
-         int maxArea = 0;
-        for (int i = 0; i < n; i++) {
-            leftMost[i] += 1;
-            rightMost[i] -= 1;
-            maxArea = Math.max(maxArea, heights[i] * (rightMost[i] - leftMost[i] + 1));
+        int maxArea = 0;
+        for (int i = 0; i < len; i++) {
+            
+            lheights[i] += 1;
+            rheights[i] -= 1;
+            maxArea = Math.max(maxArea, heights[i] * (rheights[i] - lheights[i] + 1));
         }
         return maxArea;
+           
+        // 2(pop) 1 5(pop) 6(pop) 2 3
+        // l[] = [2, 0, 1, 5, 1, 2]
     }
 }
-
+//    2 1 5 6 2 3
+// p  2 
+// s    
 // largestRect = 2
-
-// heights = [2,1,5,6,2,3]
-//                i
-//         min(1, tos) = 2 + 1 = 4
-//         add to stack
-//         max(3, 5)
-//         Math.max(prevRect, curr, min(curr, tos) * 2)
-//         currMin = Math.min(x, x) = 1
-// stack = [2 1 5 10]
-           
 // l = 5
 // 2(5)
