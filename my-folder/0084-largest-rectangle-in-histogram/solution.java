f38b5e4371a938045be42aab52c256137eb481c0
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-01-22 22:35:53 +0000

    Sync LeetCode submission Runtime - 106 ms (29.19%), Memory - 77.9 MB (62.18%)

--- my-folder/0084-largest-rectangle-in-histogram/solution.java
+++ my-folder/0084-largest-rectangle-in-histogram/solution.java
@@ -5,7 +5,7 @@ class Solution {
         int[] rheights = new int[len];
         Stack<Integer> stack = new Stack<>();
         
-        // prefix array checking the min 
+        // prefix array checking the min of the element to the left 
         for(int i = 0; i < len; i++){
             int curr = heights[i];
             lheights[i] = -1;
@@ -20,6 +20,7 @@ class Solution {
         
         stack.clear();
 
+        // suffix arrray checking the min element from  the right
         for(int i = len - 1; i >= 0; i--){
             int curr = heights[i];
             rheights[i] = len;
@@ -35,20 +36,23 @@ class Solution {
 
         int maxArea = 0;
         for (int i = 0; i < len; i++) {
-            
             lheights[i] += 1;
             rheights[i] -= 1;
             maxArea = Math.max(maxArea, heights[i] * (rheights[i] - lheights[i] + 1));
         }
-        return maxArea;
-           
-        // 2(pop) 1 5(pop) 6(pop) 2 3
-        // l[] = [2, 0, 1, 5, 1, 2]
+        return maxArea;         
     }
 }
-//    2 1 5 6 2 3
-// p  2 
-// s    
-// largestRect = 2
-// l = 5
-// 2(5)
+// 2(pop) 1 5(pop) 6(pop) 2 3
+        // l[] = [2, -1, 1, 5, 1, 2]
+                         
+        // stack = [3(pop), 2(pop), 6(pop), 5(pop), 1, 2]
+        // r[] = [1, 7, 2, 2, 7, 7]
+        //    r[] = [1, 7, 4, 4, 7, 7]
+        //    l[] = [-1, -1, 1, 2, 1, 5]
+        // maxArea(2) = 2 * 0 - 0 + 1 = 2
+        //         = 1 * 3 - 2 + 1 = 2
+        //         = 5 * 6 - 5 + 1 = 10
+        //         = 6 * 5 - 5 + 1 = 6
+        //         = 2  = 2
+        //         = 3 * (3 - 3 + 1) = 3 
