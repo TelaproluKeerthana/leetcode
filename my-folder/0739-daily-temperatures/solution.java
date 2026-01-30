8a1db2c3d8cd11de3f1347c488ba0ba2b660ee69
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-01-30 22:42:47 +0000

    Sync LeetCode submission Runtime - 22 ms (96.04%), Memory - 103.9 MB (82.22%)

--- my-folder/0739-daily-temperatures/solution.java
+++ my-folder/0739-daily-temperatures/solution.java
@@ -4,13 +4,38 @@ class Solution {
         int[] result = new int[temperatures.length];
         for(int i = 0; i < temperatures.length; i++){
             while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
-                //update result array when you see a higher temperature
-                result[stack.peek()] = i - stack.pop();
+                int top = stack.pop();
+                result[top] = i - top; 
             }
+
             stack.push(i);
         }
-    
 
     return result;
     }
 }
+
+
+
+
+// res = [1, 1, 4, 2, 1, 1, 0, 0]
+//        73,74,75,71,69,72,76,73
+
+// while(74 > temp[stack.peek()]) = 74 > 73 = currIdx - stack.peek() = 1-0
+
+// res[0] = 1
+// res[4] = 5  - 4
+// res[3] = 5 - 3
+// res[5] = 6 - 5
+// res[2] = 6 - 2
+// once you iterate over all numbers then you return 
+// stk = [0(pop), 1(pop), 2(pop), 3(pop), 4(pop), 5(pop), 6]
+                     
+
+
+
+
+
+
+
+
