9352cf9a9e922c1ff6d63a90224aee1e070fd9eb
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-02-26 00:51:37 +0000

    Sync LeetCode submission Runtime - 2 ms (30.12%), Memory - 43.2 MB (46.73%)

--- my-folder/0520-detect-capital/solution.java
+++ my-folder/0520-detect-capital/solution.java
@@ -1,44 +1,10 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        if(word.length() == 1) return true;
+     if(word.equals(word.toUpperCase())) return true;
+     if(word.equals(word.toLowerCase())) return true;
+     if(Character.isUpperCase(word.charAt(0)) && 
+     (word.substring(1).equals(word.substring(1).toLowerCase()))) return true;
 
-        char curr = word.charAt(0);
-        boolean lowercase = true;
-        if((word.charAt(0) >= 65 && (int)curr <= 90) && (Character.isLowerCase(word.charAt(1)))){
-            lowercase = true;
-        }
-        else if((int)curr >= 65 && (int)curr <= 90){
-            lowercase = false;
-        }
-        else{
-            lowercase = true;
-        }
-
-        for(int i = 1; i < word.length(); i++){
-            char c = word.charAt(i);
-            if(lowercase == true){
-                if(lowerCaseBounds(c)) continue;
-                else return false;
-            }
-            else{
-                if(upperCaseBounds(c)) continue;
-                else return false;
-            }
-        }
-
-        return true;  
-    }
-
-    public boolean lowerCaseBounds(char c){
-        if((int) c >= 97){
-            return true;
-        }
-        return false;
-    }
-    public boolean upperCaseBounds(char c){
-        if((int) c >= 65 && (int) c <= 90){
-            return true;
-        }
-        return false;
+     return false;  
     }
 }
