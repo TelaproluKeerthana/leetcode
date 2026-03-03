0f8e04946aa5de7987042dafbc96aebff3e76ac3
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-03 17:25:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.3 MB (76.22%)

--- my-folder/0075-sort-colors/solution.java
+++ my-folder/0075-sort-colors/solution.java
@@ -1,25 +1,38 @@
 class Solution {
     public void sortColors(int[] nums) {
         int left = 0;
-        int curr = 0;
         int right = nums.length - 1;
+        
+        int curr = 0;
         while(curr <= right){
-            //all the 0's are to be stored in the front
             if(nums[curr] == 0){
                 int temp = nums[left];
                 nums[left++] = nums[curr];
                 nums[curr++] = temp;
             }
-            // all 2's should be at the end, curr should be incremented only when the we encounter a 0, 
             else if(nums[curr] == 2){
                 int temp = nums[right];
                 nums[right--] = nums[curr];
-                nums[curr] = temp;
+                nums[curr] = temp;  
             }
-            // this is the care where 1's, we move to next element as 1's are supposed to be in the center
             else{
                 curr++;
             }
         }
+
     }
 }
+
+// l = 0
+// r = 6
+
+// [2,0,2,1,1,0]
+//  l         r
+
+// [0 0 1 1 2 2]
+//      l    
+//        r
+           
+// swap 2 0
+
+
