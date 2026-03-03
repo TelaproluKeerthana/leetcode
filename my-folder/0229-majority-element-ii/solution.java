0cb06658370c0ed72177e1662d234bb8a07d8957
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-03 18:32:31 +0000

    Sync LeetCode submission Runtime - 2 ms (99.91%), Memory - 52.5 MB (64.42%)

--- my-folder/0229-majority-element-ii/solution.java
+++ my-folder/0229-majority-element-ii/solution.java
@@ -1,38 +1,49 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1 = 0, candidate2 = 1; // any two distinct initial values
-        int count1 = 0, count2 = 0;
-
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
+        int c1 = 0, c2 = 1;
+        int cnt1 = 0, cnt2 = 0;
+        
+        // by the end you will have 2 candidates whose
+        for(int num : nums){
+            if(c1 == num){
+                cnt1++;
+            } else if(c2 == num){
+                cnt2++;
+            }  else if(cnt1 == 0){
+                c1 = num;
+                cnt1 = 1;
+            } else if(cnt2 == 0){
+                c2 = num;
+                cnt2 = 1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
 
-        // Second pass: validate counts
-        count1 = count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+
+        // check the counts of c1 anc c2 
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int num : nums){
+            if(num == c1) cnt1++;
+            else if(num == c2) cnt2++;
         }
 
         List<Integer> result = new ArrayList<>();
-        int threshold = nums.length / 3;
-        if (count1 > threshold) result.add(candidate1);
-        if (count2 > threshold) result.add(candidate2);
+        int midLen = nums.length / 3;
+        if(cnt1  > midLen){
+            result.add(c1);
+        }
+        if(cnt2  > midLen){
+            result.add(c2);
+        }
 
         return result;
     }
 }
+
+
+// for any array there can be atmost 2 candidates who have > n/3 occurances
 
