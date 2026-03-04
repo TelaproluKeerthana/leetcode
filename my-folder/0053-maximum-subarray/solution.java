66d14db3ef0526c7a991f78ea635526d613bd09f
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-04 21:59:06 +0000

    Sync LeetCode submission Runtime - 1 ms (99.96%), Memory - 77.3 MB (47.97%)

--- my-folder/0053-maximum-subarray/solution.java
+++ my-folder/0053-maximum-subarray/solution.java
@@ -1,42 +1,22 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-       return divide(nums, 0, nums.length - 1);
+        int currSum = 0;
+        int maxSum = nums[0];
+
+        for(int num : nums){
+            currSum += num;
+            maxSum = Math.max(currSum, maxSum);
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+
+        return maxSum;
     }
-
-    private int divide(int[] nums, int left, int right){
-        if(left == right){
-            return nums[left];
-        }
-
-        int mid = left + (right - left) / 2;
-        int leftMax = divide(nums, left, mid);
-        int rightMax = divide(nums, mid + 1, right);
-        int crossMax = maxCrossing(nums, left, mid, right);
-        return Math.max(Math.max(leftMax, rightMax), crossMax);
-    }
-
-    private int maxCrossing(int[] nums, int left, int mid, int right){
-        int leftSum = Integer.MIN_VALUE;
-        int sum = 0;
-        for(int i = mid; i >= left; i--){
-            sum += nums[i];
-            leftSum = Math.max(leftSum, sum);
-        }
-
-        int rightSum = Integer.MIN_VALUE;
-        sum = 0;
-        for(int i = mid + 1; i <= right; i++){
-            sum += nums[i];
-            rightSum = Math.max(rightSum, sum);
-        }
-
-        return leftSum + rightSum;
-    }    
 }
-// gm = 6
-// cs = 5
-// // [-2,1,-3,4,-1,2,1,-5,4]
-//             l 
-//                         r
-    
+// ms = 4
+// cs = 4
 
+// [-2,1,-3,4,-1,2,1,-5,4]        
+// maxSum = 6
+// cs = 6 - 5 = 1 + 4 = 5
