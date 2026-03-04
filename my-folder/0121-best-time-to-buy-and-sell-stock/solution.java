7b1be7d615d01f0d202d7d8c1fa106598b56a25b
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-04 22:16:26 +0000

    Sync LeetCode submission Runtime - 3 ms (5.68%), Memory - 94.6 MB (31.70%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.java
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) return 0;
-
-        int minBuy = Integer.MAX_VALUE;
+        int buy = prices[0];
+        int sell = 0;
         int profit = 0;
-        for(int i = 0; i < prices.length; i++){
-            if(minBuy > prices[i]){
-                minBuy = prices[i];
-            }
-            profit = Math.max(profit, prices[i] - minBuy);
+        for(int i = 1; i < prices.length; i++){
+            sell = prices[i];
+            buy = Math.min(buy, prices[i]);
+            profit = Math.max(profit, sell - buy);
         }
-        
+
         return profit;
     }
 }
