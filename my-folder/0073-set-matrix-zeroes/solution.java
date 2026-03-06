9a65f41e381774307361ef7c9fdf82050f4b943b
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-06 15:45:14 +0000

    Sync LeetCode submission Runtime - 1 ms (98.28%), Memory - 47.5 MB (61.95%)

--- my-folder/0073-set-matrix-zeroes/solution.java
+++ my-folder/0073-set-matrix-zeroes/solution.java
@@ -2,29 +2,25 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
-        boolean col0 = false;
-        for(int r = 0; r < rows; r++){
-            if(matrix[r][0] == 0){
-                col0 = true;
-            }
-            for(int c = 1; c < cols; c++){
-                if(matrix[r][c] == 0){
-                    matrix[0][c] = 0;
-                    matrix[r][0] = 0;
-                }  
+
+        boolean[] zeroRows = new boolean[rows];
+        boolean[] zeroCols = new boolean[cols];
+
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                if(matrix[row][col] == 0){
+                    zeroRows[row] = true;
+                    zeroCols[col] = true;
+                }
             }
         }
 
-        for(int r = rows - 1; r >= 0; r--){
-            for(int c = cols - 1; c >= 1; c--){
-                if(matrix[0][c] == 0 || matrix[r][0] == 0){
-                    matrix[r][c] = 0;
+        for(int row = 0; row < rows; row++){
+            for(int col = 0; col < cols; col++){
+                if(zeroRows[row] || zeroCols[col]){
+                    matrix[row][col] = 0;
                 }
-            }
-            if(col0){
-                    matrix[r][0] = 0;
             }
         }
     }
 }
-
