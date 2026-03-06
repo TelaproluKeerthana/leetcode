da0f205503709f3ffd248262b1a0554b50c61bec
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-06 17:03:51 +0000

    Sync LeetCode submission Runtime - 1 ms (98.28%), Memory - 47.8 MB (25.38%)

--- my-folder/0073-set-matrix-zeroes/solution.java
+++ my-folder/0073-set-matrix-zeroes/solution.java
@@ -2,25 +2,34 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
+        
+        // we track the intersection of row0 and col0 seperately using this flag 
+        int col0 = 1;
 
-        boolean[] zeroRows = new boolean[rows];
-        boolean[] zeroCols = new boolean[cols];
-
+       // update all the first row and col positions to 0 when we encounter a zero 
         for(int row = 0; row < rows; row++){
-            for(int col = 0; col < cols; col++){
+           
+            if(matrix[row][0] == 0) col0 = 0;
+
+            for(int col = 1; col < cols; col++){
                 if(matrix[row][col] == 0){
-                    zeroRows[row] = true;
-                    zeroCols[col] = true;
+                    matrix[row][0] = 0;
+                    matrix[0][col] = 0;
                 }
             }
         }
 
-        for(int row = 0; row < rows; row++){
-            for(int col = 0; col < cols; col++){
-                if(zeroRows[row] || zeroCols[col]){
+        // update from last index position to avoid overwriting markers  
+        for(int row = rows - 1; row >= 0; row--){
+            for(int col = cols - 1; col >= 1; col--){
+                if(matrix[row][0] == 0 || matrix[0][col] == 0){
                     matrix[row][col] = 0;
                 }
             }
+
+            if(col0 == 0) matrix[row][0] = 0;
         }
     }
 }
+
+
