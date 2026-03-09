a48e1619eb5e11c3c05c78d9de5dec915128f8db
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-09 18:33:36 +0000

    Sync LeetCode submission Runtime - 60 ms (9.12%), Memory - 134.1 MB (5.25%)

--- my-folder/0146-lru-cache/solution.java
+++ my-folder/0146-lru-cache/solution.java
@@ -1,21 +1,20 @@
 class ListNode{
-    int value;
+    int val;
     int key;
-    ListNode prev;
-    ListNode next;
-    public ListNode( int key, int value){
+    ListNode prev = null;
+    ListNode next = null;
+    public ListNode(int key, int val){
         this.key = key;
-        this.value = value;
+        this.val = val;
     }
 }
 
 class LRUCache {
-    ListNode left;
-    ListNode right; 
-    int size;
-
+    int maxCapacity = 0;
     Map<Integer, ListNode> lru;
-
+    ListNode left;
+    ListNode right;
+    int currCap = 0;
     public LRUCache(int capacity) {
         left = new ListNode(0, 0);
         right = new ListNode(0, 0);
@@ -23,55 +22,58 @@ class LRUCache {
         left.next = right;
         right.prev = left;
         right.next= null;
-        size = capacity;
         lru = new HashMap<>();
+        maxCapacity = capacity;
     }
     
     public int get(int key) {
-        if(lru.containsKey(key)){
-            ListNode curr = lru.get(key);
-            delete(curr);
-            insert(curr);
-            return curr.value;
+        if(!lru.containsKey(key)){
+            return -1;
         }
 
-        return -1;
+        ListNode curr = lru.get(key);
+        deleteNode(curr);
+        insertNode(curr);
+        return curr.val;
     }
     
     public void put(int key, int value) {
         if(lru.containsKey(key)){
-           ListNode node = lru.get(key);
-           delete(node);
+            ListNode curr = lru.get(key);
+            deleteNode(curr);
         }
-
-        ListNode newNode = new ListNode(key, value);
-        lru.put(key, newNode);
-        insert(newNode);
-
-        if(size < lru.size()){
-            ListNode curr = left.next;
-            delete(curr);
-            lru.remove(curr.key);
-        }
-    }
     
-    // break the link and insert before the lastNode
-    public void insert(ListNode node){
-        ListNode prev = right.prev;
-        prev.next = node;
-        node.prev = prev;
+            ListNode newNode = new ListNode(key, value);
+            lru.put(key, newNode);
+            insertNode(newNode);
+
+           
+            if(maxCapacity < lru.size()){
+                ListNode now = left.next;
+                deleteNode(now);
+                lru.remove(now.key);
+            }
+    }
+
+    public void insertNode(ListNode node){
+        ListNode curr = right.prev;
+        curr.next = node;
+        node.prev = curr;
         node.next = right;
         right.prev = node;
     }
 
-
-    public void delete(ListNode node){
-        ListNode prev = node.prev;
-        ListNode newnext = node.next;
-        prev.next = newnext;
-        newnext.prev = prev;       
+    public void deleteNode(ListNode node){
+        ListNode front = node.prev;
+        ListNode back = node.next;
+        front.next = back;
+        back.prev = front;
     }
 }
+
+// first
+// 1 : 1, 3 : 3, 4 : 4  -> if size exceeds then pop the first element
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:
