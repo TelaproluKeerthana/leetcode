b00a73fe6d1ab7b7dbe6192d2b7565c60dcbb1ac
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-15 18:52:42 +0000

    Sync LeetCode submission Runtime - 2 ms (70.19%), Memory - 47.3 MB (51.98%)

--- my-folder/0152-maximum-product-subarray/solution.java
+++ my-folder/0152-maximum-product-subarray/solution.java
@@ -1,27 +1,17 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int prefix = 0, suffix = 0;
-        int len = nums.length;
-        int res = nums[0];
-        for(int i = 0; i < len; i++){
-            prefix = nums[i] * (prefix == 0 ? 1 : prefix);
-            suffix = nums[len - 1 - i] * (suffix == 0 ? 1 : suffix);
-            res = Math.max(res, Math.max(prefix, suffix));
+        int currMin = 1;
+        int currMax = 1;
+        int maxProduct = Integer.MIN_VALUE;
+        for(int num : nums){
+            int temp = num * currMax;
+            currMax = Math.max(num, Math.max(temp, currMin * num));
+            currMin = Math.min(num, Math.min(temp, currMin * num));
+
+            maxProduct = Math.max(maxProduct, currMax);
         }
 
-        return res;
+        return maxProduct;   
     }
 }
 
-// [2,3,-2,4]
-//  res = 6
-
-//   pre = 2 * 3 = 6 suff = 4 * -2 = -8
-// [-2,0,-1]
-// pre = -2 = 0
-// suff = -1 = 0
-// res = 0
-
-// [2] [2, 3], [2, 3, -2], [2, 3, -2, 4]
-// [3] [3, -2], [3, -2, 4]
-// [4]
