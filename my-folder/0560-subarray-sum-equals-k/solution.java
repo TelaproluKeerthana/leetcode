09c0e0b08baa1b28daa078c762f7f6d73ddf6b0f
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-16 17:19:15 +0000

    Sync LeetCode submission Runtime - 29 ms (24.92%), Memory - 48.7 MB (61.15%)

--- my-folder/0560-subarray-sum-equals-k/solution.java
+++ my-folder/0560-subarray-sum-equals-k/solution.java
@@ -1,15 +1,42 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        Map<Integer, Integer> subArrayCounter = new HashMap<>(); 
-        subArrayCounter.put(0, subArrayCounter.getOrDefault(0, 1));
-        int prefixSum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        // if the diff is 0 meaning we found a valid subarray 
+        map.put(0, 1);
+
+        int currSum = 0;
         int res = 0;
-        for(int i = 0; i < nums.length; i++){
-            prefixSum += nums[i];
-            int diff = prefixSum - k;
-            res += subArrayCounter.getOrDefault(diff, 0);
-            subArrayCounter.put(prefixSum, subArrayCounter.getOrDefault(prefixSum, 0) + 1);
+        for(int num  : nums){
+            currSum += num;
+            int diff = currSum - k;
+            res += map.getOrDefault(diff, 0);
+            map.put(currSum, map.getOrDefault(currSum, 0) + 1);
         }
+
         return res;
     }
 }
+// hm = {0 : 1, 1 : 1, 2 : 1}
+
+// nums = [1,1,1]
+//             i
+// i = 0
+// cs = 1
+// res = 0
+// diff = 1 - k = -1
+// res += get(diff)
+
+// i = 1
+// cs = 2
+// diff = 2 - 2 = 0
+// res = 1
+// hm[cs] = 1 + hm[cs]
+     
+// i = 2
+// cs = 3
+// diff = 3 - 2 = 1
+// res += hm[diff] = 2
+// hm[3] = 1 + hm[3]
+
+
+
