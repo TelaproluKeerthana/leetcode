60165a1a6072ab1d63a75565828a911350fbfeb7
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-16 15:23:32 +0000

    Sync LeetCode submission Runtime - 2 ms (95.24%), Memory - 72.1 MB (40.76%)

--- my-folder/0238-product-of-array-except-self/solution.java
+++ my-folder/0238-product-of-array-except-self/solution.java
@@ -1,18 +1,25 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] result = new int[nums.length];
-        result[0] = 1;
-        
-        for(int i = 1; i < nums.length; i++){
-            result[i] = result[i - 1] * nums[i - 1];
-        }
-        
-        int suffix = 1;
-        for(int i = nums.length - 1; i >= 0; i--){
+      int[] result = new int[nums.length];
+      result[0] = 1;
+
+      for(int i = 1; i < nums.length; i++){
+        result[i] = result[i - 1] * nums[i - 1];
+      }  
+      
+
+      int suffix = 1;
+      for(int i = nums.length - 1; i >= 0; i--){
             result[i] *= suffix;
-            suffix *= nums[i] ;
-        }
-            
-        return result;
+            suffix *= nums[i];
+      }
+
+      return result;
     }
 }
+
+// [1,2,3,4]
+        
+// [1, 1, 2, 6] = prefix
+// [1, 1, 8, 6]
+
