3edd90a3ad47101a4cf0688135f3488c5448d038
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-18 16:05:14 +0000

    Sync LeetCode submission Runtime - 8 ms (13.93%), Memory - 49.2 MB (15.26%)

--- my-folder/0004-median-of-two-sorted-arrays/solution.java
+++ my-folder/0004-median-of-two-sorted-arrays/solution.java
@@ -1,40 +1,23 @@
 class Solution {
-    public double findMedianSortedArrays(int[] num1, int[] num2) {
-        int total = num1.length + num2.length;
-        int half = (total + 1) / 2;
-        int[] arr1 = num1;
-        int[] arr2 = num2;
-        if(num1.length > num2.length){
-            int[] temp = num1;
-            arr1 = arr2;
-            arr2 = temp;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length + nums2.length];
+        for(int i = 0; i < nums1.length; i++){
+            res[i] = nums1[i];
         }
 
-        int left = 0;
-        int right = arr1.length;
-        while(left <= right){
-            int mid = (left + right) / 2;
-            int mid2 = half - mid;
-            int aleft = mid > 0 ? arr1[mid - 1] : Integer.MIN_VALUE;
-            int aright = mid < arr1.length ? arr1[mid] : Integer.MAX_VALUE;
-            int bleft = mid2 > 0 ? arr2[mid2 - 1] : Integer.MIN_VALUE;
-            int bright = mid2 < arr2.length ? arr2[mid2] : Integer.MAX_VALUE;
-
-            if(aleft <= bright && bleft <= aright){
-                if(total % 2 != 0){
-                    return Math.max(aleft, bleft);
-                }
-
-                return (Math.max(aleft, bleft) + Math.min(bright, aright)) / 2.0;
-            }
-            else if(aleft > bright){
-                right = mid - 1;
-            }
-            else{
-                left = mid + 1;
-            }
+        int idx = nums1.length;
+        for(int i = 0; i < nums2.length; i++){
+            res[idx++] = nums2[i];
         }
 
-        return -1;
+        Arrays.sort(res);
+        int len = res.length;
+        if(res.length % 2 != 0){
+            return res[len / 2];
+        }
+        else{
+            return (double)(res[len / 2] + res[(len / 2) - 1]) / 2;
+        }
+
     }
 }
