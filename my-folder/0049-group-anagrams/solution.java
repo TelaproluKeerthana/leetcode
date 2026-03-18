7a4f0957e5636e0110a32685e26dfa8f7a089ff9
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-18 15:48:09 +0000

    Sync LeetCode submission Runtime - 19 ms (18.67%), Memory - 51.1 MB (7.85%)

--- my-folder/0049-group-anagrams/solution.java
+++ my-folder/0049-group-anagrams/solution.java
@@ -1,14 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> anagrams = new HashMap<>();
-        for(String str:strs){
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str : strs){
             int[] key = new int[26];
-            for(char c : str.toCharArray()){
-                key[c - 'a'] += 1;
+            for(int i = 0; i < str.length(); i++){
+                char curr = str.charAt(i);
+                key[curr - 'a'] += 1;
             }
+
             String keyStr = Arrays.toString(key);
-            anagrams.computeIfAbsent(keyStr, k -> new ArrayList<>()).add(str);   
+            map.computeIfAbsent(keyStr, k -> new ArrayList()).add(str);
         }
-        return new ArrayList<>(anagrams.values());
+
+        List<List<String>> result = new ArrayList<>();
+        for(List<String> val : map.values()){
+            result.add(val);
+        }
+
+        return result;
+
     }
 }
+
+// ["eat","tea","tan","ate","nat","bat"]
+
