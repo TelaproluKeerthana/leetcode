db9123f5a1fe8bfbe44d39c524731c5579af8115
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-19 20:40:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 47.9 MB (38.29%)

--- my-folder/0042-trapping-rain-water/solution.java
+++ my-folder/0042-trapping-rain-water/solution.java
@@ -1,28 +1,38 @@
 class Solution {
     public int trap(int[] height) {
-        int left = 0, right = height.length - 1;
-        int leftMax = 0, rightMax = 0, waterTrapped = 0;
+        int lmax = 0, rmax =  0, waterTrapped = 0;
+        int left = 0;
+        int right = height.length - 1;
+
         while(left < right){
             if(height[left] < height[right]){
-                if(height[left] >= leftMax){
-                    leftMax = height[left];
+                if(height[left] >= lmax){
+                    lmax = height[left];
                 }
                 else{
-                    waterTrapped += leftMax - height[left];
+                    waterTrapped += lmax - height[left];
                 }
                 left++;
             }
             else{
-                if(height[right] >= rightMax){
-                    rightMax = height[right];
+                if(height[right] >= rmax){
+                    rmax = height[right];
                 }
                 else{
-                    waterTrapped += rightMax - height[right];
+                    waterTrapped += rmax - height[right];
                 }
                 right--;
-            }  
+            }
         }
 
         return waterTrapped;
     }
 }
+
+//                      R
+// [0,1,0,2,1,0,1,3,2,1,2,1]
+//    L
+
+// [0 1 1 2 2 2 2 3 3 3 3 3] - PRE
+// [3,3,3,3,3,3,3,3,2,2,2,1] - SUFF
+// [0 1 1 2 2 2 2 0 2 2 2 1] - SUB
