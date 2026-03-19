a35efe7af13e66a474f2985969ebcd77923450c9
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-19 21:15:51 +0000

    Sync LeetCode submission Runtime - 12 ms (84.58%), Memory - 47.8 MB (31.40%)

--- my-folder/0347-top-k-frequent-elements/solution.java
+++ my-folder/0347-top-k-frequent-elements/solution.java
@@ -1,29 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> counter = new HashMap<>();
-        ArrayList<Integer> alist = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for(int num : nums){
-            counter.put(num, 1 + counter.getOrDefault(num , 0));
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<int[]> que = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        //  PriorityQueue<int[]> que = new PriorityQueue<>(Collections.reverseOrder());
-        for(Map.Entry<Integer, Integer> curr : counter.entrySet()){
-            que.offer(new int[]{curr.getValue(), curr.getKey()});
+        PriorityQueue<int[]> pq = new PriorityQueue<>(k, (a, b) -> b[0] - a[0]);
+        for(int key : map.keySet()){
+            pq.offer(new int[]{map.get(key), key});
         }
 
         int[] res = new int[k];
         int idx = 0;
-        while(k > 0){
-            int[] curr = que.poll();
-            res[idx++] = curr[1];
-            k--;
+        while(idx < k){
+            res[idx++] = pq.poll()[1];
         }
-
+    
         return res;
     }
 }
-
-// 1 : 3
-// 2 : 2
-// 3 : 1
