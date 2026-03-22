087c553ecd661db552012f4f1bd06c4f16770051
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-03-22 00:04:13 +0000

    Sync LeetCode submission Runtime - 4 ms (21.45%), Memory - 43.1 MB (71.45%)

---
+++ my-folder/0763-special-binary-string/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String makeLargestSpecial(String s) {
+        int count = 0, i = 0;
+        List<String> results = new ArrayList<>();
+        
+        for (int j = 0; j < s.length(); ++j) {
+            if (s.charAt(j) == '1') count++;
+            else count--;
+            
+            if (count == 0) {
+                results.add("1" + makeLargestSpecial(s.substring(i + 1, j)) + "0");
+                i = j + 1;
+            }
+        }
+        
+        Collections.sort(results, Collections.reverseOrder());
+        
+        return String.join("", results);
+    }
+}
