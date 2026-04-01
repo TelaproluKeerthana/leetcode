5aa938aac2b104e922914a9bf934b15b91785d03
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-04-01 00:00:36 +0000

    Sync LeetCode submission Runtime - 5 ms (86.54%), Memory - 44 MB (90.88%)

--- my-folder/0151-reverse-words-in-a-string/solution.java
+++ my-folder/0151-reverse-words-in-a-string/solution.java
@@ -1,15 +1,41 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.trim().split("\\s+");
-        StringBuilder res = new StringBuilder();
+        char[] arr = s.toCharArray();
 
-        for (int i = words.length - 1; i >= 0; i--) {
-            res.append(words[i]);
-            if (i != 0) {
-                res.append(" ");
+        reverse(arr, 0, arr.length - 1);
+
+        int start = 0;
+
+        for(int end = 0; end <= arr.length; end++){
+            if( end == arr.length || arr[end] == ' '){
+                reverse(arr, start, end - 1);
+                start = end + 1;
             }
         }
 
-        return res.toString();
+        return cleanSpaces(arr);
+    }
+
+    public void reverse(char[] arr, int start, int end){
+        while(start < end){
+            char temp = arr[start];
+            arr[start++] = arr[end];
+            arr[end--] = temp;
+        }
+    }
+
+    private String cleanSpaces(char[] arr){
+        int i = 0, j = 0, n = arr.length;
+        while(j  < n){
+            while(j < n && arr[j] == ' ') j++;
+
+            while(j < n && arr[j] != ' ') arr[i++] = arr[j++];
+
+            while(j < n && arr[j] == ' ') j++;
+
+            if(j < n) arr[i++] = ' ';
+        }
+
+        return new String(arr, 0, i);
     }
 }
