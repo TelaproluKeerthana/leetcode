5bc959696bda653d1bbbe8c546383dbb717ccf1c
TelaproluKeerthana <59163693+TelaproluKeerthana@users.noreply.github.com>
2026-04-27 14:48:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44 MB (23.31%)

--- my-folder/0074-search-a-2d-matrix/solution.java
+++ my-folder/0074-search-a-2d-matrix/solution.java
@@ -1,38 +1,11 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int top = 0;
-        int bot = matrix.length - 1;
-        int cols = matrix[0].length;
-        while(top <= bot){
-            int row = (top + bot) / 2;
-            if(matrix[row][cols - 1] < target){
-                top = row + 1;
-            }
-            else if(matrix[row][0] > target){
-                bot = row - 1;
-            }
-            else{
-                break;
-            }
-        }
-
-        if (!(top <= bot)) {
-        return false;
-        } 
-
-         int row = (top + bot) / 2;
-        int l = 0, r = cols - 1;
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (target > matrix[row][m]) {
-                l = m + 1;
-            } else if (target < matrix[row][m]) {
-                r = m - 1;
-            } else {
-                return true;
-            }
+        int row = 0, col = matrix[0].length - 1;
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] > target) col--;
+            else row++;
         }
         return false;
-        
     }
 }
